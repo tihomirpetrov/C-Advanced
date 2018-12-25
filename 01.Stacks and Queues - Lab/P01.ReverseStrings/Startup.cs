5f2a716b12fe8e275983b6b5cfa0c4d6acbc8919
tihomirpetrov <tihomirpetrov79@gmail.com>
2018-12-25 22:45:31 +0200

    initial commit

---
+++ 01.Stacks and Queues - Lab/P01.ReverseStrings/Startup.cs
@@ -0,0 +1,25 @@
+﻿namespace P01.ReverseStrings
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var charFromInput in input)
+            {
+                stack.Push(charFromInput);
+            }
+
+            while (stack.Count != 0)
+            {
+                Console.Write(stack.Pop());
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 01.Stacks and Queues - Lab/P06.TrafficJam/Startup.cs
@@ -0,0 +1,36 @@
+﻿namespace P06.TrafficJam
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            int carsInGreenLight = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            Queue<string> carsQueue = new Queue<string>();
+            int carsCount = 0;
+
+            while (input != "end")
+            {
+                if (input == "green")
+                {
+                    int carsThatCanPass = Math.Min(carsQueue.Count, carsInGreenLight);
+                    for (int i = 0; i < carsThatCanPass; i++)
+                    {
+                        Console.WriteLine($"{carsQueue.Dequeue()} passed!");
+                        carsCount++;
+                    }
+                }
+                else
+                {
+                    carsQueue.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{carsCount} cars passed the crossroads.");
+        }
+    }
+}
