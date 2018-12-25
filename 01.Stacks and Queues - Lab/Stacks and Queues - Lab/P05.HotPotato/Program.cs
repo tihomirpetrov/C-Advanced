afa85e48c220f3facd2d0a1f7e04dff70dbfd172
tihomirpetrov <tihomirpetrov79@gmail.com>
2018-12-25 21:30:50 +0200

    initial commit

---
+++ 01.Stacks and Queues - Lab/Stacks and Queues - Lab/P05.HotPotato/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace P05.HotPotato
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        public static void Main()
+        {
+            string[] children = Console.ReadLine().Split();
+            int tossLimit = int.Parse(Console.ReadLine());
+            List<int> list = new List<int>();
+            Stack<int> stack = new Stack<int>(list);
+            Queue<string> queue = new Queue<string>(children);
+
+            while (queue.Count != 1)
+            {
+                for (int i = 1; i < tossLimit; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {queue.Dequeue()}");
+            }
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
