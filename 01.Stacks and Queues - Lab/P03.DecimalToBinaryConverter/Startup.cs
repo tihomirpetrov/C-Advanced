7c01fab464938fcd457cba39feacf7bcdf0d81c3
tihomirpetrov <tihomirpetrov79@gmail.com>
2018-12-26 13:58:50 +0200

    initial commit

---
+++ 01.Stacks and Queues - Lab/P03.DecimalToBinaryConverter/Startup.cs
@@ -0,0 +1,31 @@
+﻿namespace P03.DecimalToBinaryConverter
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            Stack<int> numToBinary = new Stack<int>();
+
+            if (number == 0)
+            {
+                Console.WriteLine(0);
+            }
+
+            while (number != 0)
+            {
+                numToBinary.Push(number % 2);
+                number /= 2;
+            }
+
+            while (numToBinary.Count > 0)
+            {
+                Console.Write(numToBinary.Pop());
+            }
+            Console.WriteLine();
+        }
+    }
+}
