9a67f02520febf9a8269bde3a8afc53ccb7eaeff
tihomirpetrov <tihomirpetrov79@gmail.com>
2018-12-26 14:07:16 +0200

    initial commit

---
+++ 01.Stacks and Queues - Lab/P04.MatchingBrackets/Startup.cs
@@ -0,0 +1,29 @@
+﻿namespace P04.MatchingBrackets
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    stack.Push(i);
+                }
+
+                if (input[i] == ')')
+                {
+                    int startIndex = stack.Pop();
+                    string reminder = input.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(reminder);
+                }
+            }
+        }
+    }
+}
