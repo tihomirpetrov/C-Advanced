4f9faac59863033e8020e3e8d943b48cb5650a0f
tihomirpetrov <tihomirpetrov79@gmail.com>
2018-12-27 22:17:21 +0200

    initial commit

---
+++ 01.Stacks and Queues - Exercise/P03.MaximumElement/Startup.cs
@@ -0,0 +1,29 @@
+﻿namespace P03.MaximumElement
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<int> numbers = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] sequence = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int j = 0; j < sequence.Length; j++)
+                {
+                    numbers.Push(sequence[j]);
+                }
+
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(numbers.Max());
+                }
+            }
+        }
+    }
+}
