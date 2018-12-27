81a9e992c21e88df452da2ac412d1c0d0139f96f
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-12-27 10:51:36 +0200

    initial commit

---
+++ 01.Stacks and Queues - Exercise/P01.ReverseNumbersWithAStack/Startup.cs
@@ -0,0 +1,28 @@
+﻿namespace P01.ReverseNumbersWithAStack
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            int[] numbers = input.Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>();
+
+            foreach (var num in numbers)
+            {
+                stack.Push(num);
+            }
+
+            while (stack.Count != 0)
+            {
+                Console.Write(stack.Pop());
+                Console.Write(' ');
+            }
+            Console.WriteLine();
+        }
+    }
+}
