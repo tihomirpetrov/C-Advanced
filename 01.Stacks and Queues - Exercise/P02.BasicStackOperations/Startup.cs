542751be4288c853b2b54599bd86328e18c224be
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2018-12-27 11:37:28 +0200

    initial commit

---
+++ 01.Stacks and Queues - Exercise/P02.BasicStackOperations/Startup.cs
@@ -0,0 +1,37 @@
+﻿namespace P02.BasicStackOperations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+            int numbersToPush = int.Parse(input[0]);
+            int numbersToPop = int.Parse(input[1]);
+            int numbersToPeek = int.Parse(input[2]);
+            Stack<int> stack = new Stack<int>();
+            int[] sequence = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < numbersToPush; i++)
+            {
+                stack.Push(sequence[i]);
+            }
+
+            for (int i = 0; i < numbersToPop; i++)
+            {
+                stack.Pop();
+            }
+
+            for (int i = 0; i < stack.Count; i++)
+            {
+                if (numbersToPeek == stack.Peek())
+                {
+                    Console.WriteLine(true);
+                }                         
+            }
+        }
+    }
+}
