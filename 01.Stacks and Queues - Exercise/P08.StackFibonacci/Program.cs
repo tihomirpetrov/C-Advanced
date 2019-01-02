0f281d1c923006a073f86f26c6520515ac84fcd1
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-01-02 22:11:01 +0200

    initial commit

---
+++ 01.Stacks and Queues - Exercise/P08.StackFibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace P08.StackFibonacci
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Stack<long> fibonacci = new Stack<long>();
+            int n = int.Parse(Console.ReadLine());
+
+            fibonacci.Push(0);
+            fibonacci.Push(1);
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                long firstNumber = fibonacci.Pop();
+                long secondNumber = fibonacci.Peek();
+
+                fibonacci.Push(firstNumber);
+                fibonacci.Push(firstNumber + secondNumber);
+            }
+
+            Console.WriteLine(fibonacci.Peek());
+        }
+    }
+}
