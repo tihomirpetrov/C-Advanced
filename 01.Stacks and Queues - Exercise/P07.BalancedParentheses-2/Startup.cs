d0161281b83111a8648c3dfe34e7d97d727be08a
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-01-02 21:55:43 +0200

    initial commit

---
+++ 01.Stacks and Queues - Exercise/P07.BalancedParentheses-2/Startup.cs
@@ -0,0 +1,54 @@
+﻿namespace P07.BalancedParentheses_2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            Stack<char> stackOfParentheses = new Stack<char>();
+            string parentheses = Console.ReadLine();
+            char[] openParantheses = new char[] { '(', '[', '{' };
+            bool isValid = true;
+            for (int i = 0; i < parentheses.Length; i++)
+            {
+                char currentBracket = parentheses[i];
+
+                if (openParantheses.Contains(currentBracket))
+                {
+                    stackOfParentheses.Push(currentBracket);
+                    continue;
+                }
+                if (stackOfParentheses.Count == 0)
+                {
+                    isValid = false;
+                    break;
+                }
+
+                if (stackOfParentheses.Peek() == '(' && currentBracket == ')')
+                {
+                    stackOfParentheses.Pop();
+                }
+                else if (stackOfParentheses.Peek() == '[' && currentBracket == ']')
+                {
+                    stackOfParentheses.Pop();
+                }
+                else if (stackOfParentheses.Peek() == '{' && currentBracket == '}')
+                {
+                    stackOfParentheses.Pop();
+                }
+            }
+
+            if (isValid && stackOfParentheses.Count == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
