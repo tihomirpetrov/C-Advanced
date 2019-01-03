cab7367f6cd2ef78014a528b5d1712f63178ddb4
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-01-03 22:41:45 +0200

    initial commit

---
+++ 02.Multidimensional Arrays - Lab/P01.SumMatrixElements/Startup.cs
@@ -0,0 +1,43 @@
+﻿namespace P01.SumMatrixElements
+{
+    using System;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            int[] sizes = Console.ReadLine()
+                .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(Int32.Parse)
+                .ToArray();
+
+            int[,] matrix = new int[sizes[0], sizes[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] columnElements = Console.ReadLine()
+                .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(Int32.Parse)
+                .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = columnElements[col];
+                }
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+
+            int sum = 0;
+
+            foreach (var element in matrix)
+            {
+                sum += element;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
