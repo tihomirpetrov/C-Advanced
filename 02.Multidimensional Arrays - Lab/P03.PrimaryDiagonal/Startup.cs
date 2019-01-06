921912d6433511f1da475d0ff0fc954e714fb100
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-01-06 21:43:10 +0200

    initial commit

--- 02.Multidimensional Arrays - Lab/P03.PrimaryDiagonal/Startup.cs
+++ 02.Multidimensional Arrays - Lab/P03.PrimaryDiagonal/Startup.cs
@@ -1,11 +1,37 @@
 ﻿namespace P03.PrimaryDiagonal
 {
     using System;
+    using System.Linq;
+
     public class Startup
     {
         public static void Main()
         {
-           
+            int sizeOfMatrix = int.Parse(Console.ReadLine());
+            int rows = sizeOfMatrix;
+            int cols = sizeOfMatrix;
+            int[,] matrix = new int[rows, cols];
+            int sum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] columnElements = Console.ReadLine()
+                .Split()
+                .Select(Int32.Parse)
+                .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = columnElements[col];
+                }
+            }
+
+            for (int i = 0; i < sizeOfMatrix; i++)
+            {
+                sum += matrix[i,i];
+            }
+
+            Console.WriteLine(sum);           
         }
     }
 }
