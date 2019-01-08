eaa701f5e3fa6ac5ec064916bf407b3afb1fed86
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-01-08 20:48:38 +0200

    initial commit

---
+++ 02.Multidimensional Arrays - Lab/P04.SymbolInMatrix/Startup.cs
@@ -0,0 +1,45 @@
+﻿namespace P04.SymbolInMatrix
+{
+    using System;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            int sizeOfMatrix = int.Parse(Console.ReadLine());
+            int rows = sizeOfMatrix;
+            int cols = sizeOfMatrix;
+            char[,] matrix = new char[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                char[] columnElements = Console.ReadLine().ToCharArray();
+
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = columnElements[j];
+                }
+            }
+
+            string symbol = Console.ReadLine();
+            bool isSymbol = false;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i,j].ToString() == symbol)
+                    {
+                        Console.WriteLine($"{i}, {j}");
+                        isSymbol = true;
+                    }
+                }
+            }
+            if (isSymbol == false)
+            {
+                Console.WriteLine($"{symbol} does not occur in the matrix");
+            }
+        }
+    }
+}
