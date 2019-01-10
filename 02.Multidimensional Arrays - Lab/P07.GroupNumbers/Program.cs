1fbbaaaafaf22d2459a05f911b62f01026a86a1b
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-01-10 22:02:52 +0200

    initial commit

---
+++ 02.Multidimensional Arrays - Lab/P07.GroupNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace P07.GroupNumbers
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[][] jagged = new int[3][];
+            int[] sizes = new int[3];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sizes[Math.Abs(numbers[i] % 3)]++;
+            }
+
+            for (int i = 0; i < sizes.Length; i++)
+            {
+                jagged[i] = new int[sizes[i]];
+            }
+
+            int[] indices = new int[3];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int reminder = Math.Abs(numbers[i] % 3);
+                jagged[reminder][indices[reminder]++] = numbers[i];
+            }
+
+            foreach (var item in jagged)
+            {
+                Console.WriteLine(string.Join(" ", item));
+            }
+        }
+    }
+}
