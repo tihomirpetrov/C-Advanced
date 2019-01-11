1d5edb1217b0d87226069a85b0d69f924214120d
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-11 15:18:43 +0200

    initial commit

--- 02.Multidimensional Arrays - Exercise/P01.MatrixOfPalindromes/Startup.cs
+++ 02.Multidimensional Arrays - Exercise/P01.MatrixOfPalindromes/Startup.cs
@@ -1,11 +1,25 @@
 ﻿namespace P01.MatrixOfPalindromes
 {
     using System;
+    using System.Linq;
+
     public class Startup
     {
         public static void Main()
         {
-            
+            char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            int[] firstLine = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int row = firstLine[0];
+            int col = firstLine[1];
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write($"{alphabet[i]}{alphabet[i+j]}{alphabet[0+i]} ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
