6b40c1f9a44f6817bea2fa5f4a96f4473c3f6fe0
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-11 15:24:18 +0200

    initial commit

---
+++ 02.Multidimensional Arrays - Exercise/P02.DiagonalDifference/Startup.cs
@@ -0,0 +1,19 @@
+﻿namespace P02.DiagonalDifference
+{
+    using System;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            int sizeOfMatrix = int.Parse(Console.ReadLine());
+            int[][] matrix = new int[sizeOfMatrix][];
+
+            for (int i = 0; i < sizeOfMatrix; i++)
+            {
+                int[] row = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            }
+        }
+    }
+}
