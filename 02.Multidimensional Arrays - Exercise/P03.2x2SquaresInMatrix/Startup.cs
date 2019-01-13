71256c976f13f5ff31dad5f06156778c6e42bfbc
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-01-13 22:00:05 +0200

    initial commit

--- 02.Multidimensional Arrays - Exercise/P03.2x2SquaresInMatrix/Startup.cs
+++ 02.Multidimensional Arrays - Exercise/P03.2x2SquaresInMatrix/Startup.cs
@@ -1,11 +1,45 @@
 ﻿namespace P03._2x2SquaresInMatrix
 {
     using System;
+    using System.Linq;
+
     public class Startup
     {
         public static void Main()
         {
-            
+            int[] dimension = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int rows = dimension[0];
+            int cols = dimension[1];
+
+            char[,] matrix = new char[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] characters = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = characters[col];
+                }
+            }
+
+            int counter = 0;
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    bool areEqual = matrix[row, col] == matrix[row, col + 1] &&
+                        matrix[row, col] == matrix[row + 1, col + 1] &&
+                        matrix[row, col] == matrix[row + 1, col];
+
+                    if (areEqual)
+                    {
+                        counter++;
+                    }
+                }
+            }
+            Console.WriteLine(counter);
         }
     }
 }
