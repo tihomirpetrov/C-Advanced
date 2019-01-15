6aad9464abfe87621dc46351fe5d39bfdc9480a0
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-01-15 21:40:07 +0200

    initial commit

---
+++ 01.Stacks and Queues - Lab/P05.Supermarket/Startup.cs
@@ -0,0 +1,32 @@
+﻿namespace P05.Supermarket
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            Queue<string> names = new Queue<string>();
+            
+            while (input != "End")
+            {
+                if (input != "Paid")
+                {
+                    names.Enqueue(input);
+                }
+                else
+                {
+                    while (names.Count > 0)
+                    {
+                        Console.WriteLine(names.Dequeue());
+                    }                    
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{names.Count} people remaining.");
+        }
+    }
+}
