f54858a8d125cd5db9a1758f53787000a7d76b70
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-15 15:29:37 +0200

    initial commit

---
+++ 01.Stacks and Queues - Lab/P03.SimpleCalculator/Startup.cs
@@ -0,0 +1,34 @@
+﻿namespace P03.SimpleCalculator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            string[] tokens = input.Split();
+            Stack<string> elements = new Stack<string>(tokens.Reverse());
+
+            while (elements.Count > 1)
+            {
+                int firstNumber = int.Parse(elements.Pop());
+                string operand = elements.Pop();
+                int secondNumber = int.Parse(elements.Pop());
+
+                if (operand == "+")
+                {
+                    elements.Push((firstNumber + secondNumber).ToString());
+                }
+                else if (operand == "-")
+                {
+                    elements.Push((firstNumber - secondNumber).ToString());
+                }
+            }
+
+            Console.WriteLine(elements.Pop());
+        }
+    }
+}
