51f322ff4ae3eb4f14a2f22af7c09d5df2ac93ff
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-15 13:33:27 +0200

    initial commit

---
+++ 01.Stacks and Queues - Lab/P01.ReverseStrings-2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P01.ReverseStrings
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            Stack<char> myStack = new Stack<char>();
+
+            foreach (var item in input)
+            {
+                myStack.Push(item);
+            }
+
+            foreach (var item in myStack)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
