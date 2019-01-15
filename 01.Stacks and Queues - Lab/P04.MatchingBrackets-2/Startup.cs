f685290b1ce916b2469f0ce9e1a5d679dad0edb9
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-15 16:48:33 +0200

    initial commit

---
+++ 01.Stacks and Queues - Lab/P04.MatchingBrackets-2/Startup.cs
@@ -0,0 +1,28 @@
+﻿namespace P04.MatchingBrackets_2
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            Stack<int> expressions = new Stack<int>();
+            
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    expressions.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    int openBracket = expressions.Pop();
+                    string subExpression = input.Substring(openBracket, i - openBracket + 1);
+                    Console.WriteLine(subExpression);
+                }
+            }
+        }
+    }
+}
