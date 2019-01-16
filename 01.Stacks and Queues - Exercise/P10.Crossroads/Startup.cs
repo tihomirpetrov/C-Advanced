e4be27ff5a9d5bd82129e46a9c18cf979f968abc
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-01-16 21:09:22 +0200

    initial commit

--- 01.Stacks and Queues - Exercise/P10.Crossroads/Startup.cs
+++ 01.Stacks and Queues - Exercise/P10.Crossroads/Startup.cs
@@ -8,8 +8,11 @@
         public static void Main()
         {
             int durationOfGreenLight = int.Parse(Console.ReadLine());
+            int saveGreenLight = durationOfGreenLight;
             int durationOfFreeWindow = int.Parse(Console.ReadLine());
-            Queue<char> car = new Queue<char>();
+            Queue<string> car = new Queue<string>();
+            int counter = 0;
+            int damageCount = 0;
 
             string command = Console.ReadLine();
 
@@ -17,12 +20,41 @@
             {
                 if (command != "green")
                 {
-
+                    car.Enqueue(command);
                 }
                 else
                 {
-
+                    while (car.Count > 0)
+                    {
+                        string passingCar = car.Peek();
+                        if (passingCar.Length <= durationOfGreenLight + durationOfFreeWindow)
+                        {
+                            car.Dequeue();
+                            durationOfGreenLight -= passingCar.Length;
+                            counter++;
+                        }
+                        else
+                        {
+                            if (durationOfGreenLight > 0)
+                            {
+                                string damagedCar = passingCar;
+                                char hitIndex = damagedCar[durationOfGreenLight + durationOfFreeWindow];
+                                Console.WriteLine("A crash happened!");
+                                Console.WriteLine($"{damagedCar} was hit at {hitIndex}.");
+                                damageCount++;
+                            }
+                            break;
+                        }
+                    }
                 }
+                durationOfGreenLight = saveGreenLight;
+                command = Console.ReadLine();
+            }
+
+            if (car.Count > 0 && damageCount == 0)
+            {
+                Console.WriteLine("Everyone is safe.");
+                Console.WriteLine($"{counter} total cars passed the crossroads.");
             }
         }
     }
