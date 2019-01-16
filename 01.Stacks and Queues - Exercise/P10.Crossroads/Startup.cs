a1922ca2e8c90d8765210f5eb61068e525564a64
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-16 11:30:20 +0200

    initial commit

---
+++ 01.Stacks and Queues - Exercise/P10.Crossroads/Startup.cs
@@ -0,0 +1,29 @@
+﻿namespace P10.Crossroads
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            int durationOfGreenLight = int.Parse(Console.ReadLine());
+            int durationOfFreeWindow = int.Parse(Console.ReadLine());
+            Queue<char> car = new Queue<char>();
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                if (command != "green")
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
