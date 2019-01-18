ff688e15ebf3c7df8a2151662f00d7f53eac1dab
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-18 11:59:10 +0200

    initial commit

---
+++ 02.Multidimensional Arrays - Exercise/P01.DiagonalDifference/Startup.cs
@@ -0,0 +1,36 @@
+﻿namespace P01.DiagonalDifference
+{
+    using System;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            int sizeOfMatrix = int.Parse(Console.ReadLine());
+            int rows = sizeOfMatrix;
+            int cols = sizeOfMatrix;
+            int[][] matrix = new int[rows][];
+            long firstSum = 0;
+            long secondSum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                matrix[row] = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            }
+
+            for (int i = 0; i < sizeOfMatrix; i++)
+            {
+                firstSum += matrix[i][i];
+            }
+
+            for (int row = 0, col = sizeOfMatrix - 1; row < sizeOfMatrix; row++, col--)
+            {
+                secondSum += matrix[row][col];
+            }
+                    
+            long difference = Math.Abs(firstSum - secondSum);
+            Console.WriteLine(difference);
+        }
+    }
+}
