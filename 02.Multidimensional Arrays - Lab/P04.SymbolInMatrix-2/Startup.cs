05eccb80b39972c1242785a226288848665bae8d
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-18 10:11:09 +0200

    initial commit

---
+++ 02.Multidimensional Arrays - Lab/P04.SymbolInMatrix-2/Startup.cs
@@ -0,0 +1,44 @@
+﻿namespace P04.SymbolInMatrix_2
+{
+    using System;
+    
+    class Startup
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] colElements = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+
+            string symbol = Console.ReadLine();
+            bool hasSymbol = false;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col].ToString() == symbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        hasSymbol = true;
+                        return;
+                    }
+                }
+            }
+
+            if (!hasSymbol)
+            {
+                Console.WriteLine($"{symbol} does not occur in the matrix");
+            }
+        }
+    }
+}
---
+++ 02.Multidimensional Arrays - Lab/P05.SquareWithMaximumSum-2/Startup.cs
@@ -0,0 +1,57 @@
+﻿namespace P05.SquareWithMaximumSum_2
+{
+    using System;
+    using System.Linq;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            int[] sizes = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rows = sizes[0];
+            int cols = sizes[1];
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] colElements = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+
+            int maxSum = int.MinValue;
+            int maxRow = 0;
+            int maxCol = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    int currentSum = 0;
+                    currentSum += matrix[row, col] + matrix[row, col + 1] + matrix[row + 1, col] + matrix[row + 1, col + 1];
+
+                    if (currentSum > maxSum)
+                    {
+                        maxSum = currentSum;
+                        maxRow = row;
+                        maxCol = col;
+                    }
+                }
+            }
+
+            for (int i = maxRow; i < maxRow + 2; i++)
+            {
+                for (int j = maxCol; j < maxCol + 2; j++)
+                {
+                    Console.Write(string.Join(" ", matrix[i, j] + " "));
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(maxSum);
+        }
+    }
+}
