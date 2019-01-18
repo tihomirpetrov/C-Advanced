e41027bbc3e35609c36ae102c7af57a627544fb2
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-01-18 21:37:28 +0200

    initial commit

---
+++ 02.Multidimensional Arrays - Exercise/P02.SquaresInMatrix/Startup.cs
@@ -0,0 +1,41 @@
+﻿namespace P02.SquaresInMatrix
+{
+    using System;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            int[] sizes = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rows = sizes[0];
+            int cols = sizes[1];
+            char[,] matrix = new char[rows, cols];
+
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] colElements = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+
+            int counter = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1] && matrix[row, col] == matrix[row + 1, col] && matrix[row, col] == matrix[row + 1, col +1])
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
