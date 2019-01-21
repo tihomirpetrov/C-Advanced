fc7b7fe3f0bc8fbf029f76472cf0e6791f2d0e40
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-01-22 01:01:21 +0200

    initial commit

---
+++ 02.Multidimensional Arrays - Exercise/P08.Bombs/Startup.cs
@@ -0,0 +1,25 @@
+﻿namespace P08.Bombs
+{
+    using System;
+    using System.Linq;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int rows = n;
+            int cols = n;
+            int[][] matrix = new int[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                matrix[row] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            }
+
+            string input = Console.ReadLine();
+
+            string[] bombCoordinates = input.Split(',', StringSplitOptions.RemoveEmptyEntries).ToArray();
+        }
+    }
+}
