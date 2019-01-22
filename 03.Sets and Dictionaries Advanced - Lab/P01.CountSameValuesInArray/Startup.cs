7b3b2d77cebe6c82f8e99657e2d0d429259cdd00
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-22 08:49:19 +0200

    update readme

--- 03.Sets and Dictionaries Advanced - Lab/P01.CountSameValuesInArray/Startup.cs
+++ 03.Sets and Dictionaries Advanced - Lab/P01.CountSameValuesInArray/Startup.cs
@@ -1,12 +1,33 @@
 ﻿namespace P01.CountSameValuesInArray
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     class Startup
     {
         public static void Main()
         {
-           
+            string[] input = Console.ReadLine().Split().ToArray();
+            Dictionary<double, List<int>> numbers = new Dictionary<double, List<int>>();
+            int counter = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                double number = double.Parse(input[i]);
+                if (!numbers.ContainsKey(number))
+                {
+                    numbers.Add(number, new List<int>());
+                    counter++;
+                }
+                numbers[number].Add(counter);
+            }
+
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(string.Join(" ", $"{item.Key} - {item.Value.Count()} times"));
+            }
         }
     }
 }
