79df4bff1eea5b6eea7c84077fa31803b28d1cd7
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-24 15:44:31 +0200

    initial commit

--- 03.Sets and Dictionaries Advanced - Lab/P07.SoftUniParty/Startup.cs
+++ 03.Sets and Dictionaries Advanced - Lab/P07.SoftUniParty/Startup.cs
@@ -10,33 +10,46 @@
             string input = Console.ReadLine();
             HashSet<string> vipGuests = new HashSet<string>();
             HashSet<string> regularGuests = new HashSet<string>();
-
-
+            bool isParty = false;
             while (input != "END")
             {
-
                 if (input == "PARTY")
                 {
-                    if (input.Length == 8)
+                    isParty = true;
+                    input = Console.ReadLine();
+                }
+
+                if (input.Length == 8 && isParty == false)
+                {
+                    if (input[0] == '1' || input[0] == '2' || input[0] == '3' || input[0] == '4' || input[0] == '5' || input[0] == '6' || input[0] == '7' || input[0] == '8' || input[0] == '9')
                     {
-                        if (input[0] == '1' || input[0] == '2' || input[0] == '3' || input[0] == '4' || input[0] == '5' || input[0] == '6' || input[0] == '7' || input[0] == '8' || input[0] == '9')
+                        if (!vipGuests.Contains(input))
                         {
                             vipGuests.Add(input);
                         }
-                        else
+                    }
+                    else
+                    {
+                        if (!regularGuests.Contains(input))
                         {
                             regularGuests.Add(input);
                         }
                     }
-                    input = Console.ReadLine();
-
                 }
-                else
-                {
-                    input = Console.ReadLine();
-
+                
+                else if(isParty)
+                {              
+                    if (vipGuests.Contains(input))
+                    {
+                        vipGuests.Remove(input);
+                    }
+                    else if (regularGuests.Contains(input))
+                    {
+                        regularGuests.Remove(input);
+                    }
                 }
 
+                input = Console.ReadLine();
             }
 
             int count = vipGuests.Count + regularGuests.Count;
