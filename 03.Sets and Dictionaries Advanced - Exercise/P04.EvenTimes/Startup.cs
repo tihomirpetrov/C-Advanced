4ba769db3c4955f232166470c7258a5978972306
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-24 16:56:41 +0200

    initial commit

---
+++ 03.Sets and Dictionaries Advanced - Exercise/P04.EvenTimes/Startup.cs
@@ -0,0 +1,27 @@
+﻿namespace P04.EvenTimes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<int> numbers = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                numbers.Add(number);
+
+            }
+
+            foreach (var number in numbers.Where(x => x % 2 != 0))
+            {
+                Console.WriteLine(Math.Abs(number));
+            }
+        }
+    }
+}
