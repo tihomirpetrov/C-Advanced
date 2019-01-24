8e8acd5c93d513439bce2aba0292a22ece10bbb9
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-24 12:00:18 +0200

    initial commit

---
+++ 03.Sets and Dictionaries Advanced - Lab/P07.SoftUniParty/Startup.cs
@@ -0,0 +1,50 @@
+﻿namespace P07.SoftUniParty
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            HashSet<string> vipGuests = new HashSet<string>();
+            HashSet<string> regularGuests = new HashSet<string>();
+
+
+            while (input != "END")
+            {
+
+                if (input == "PARTY")
+                {
+                    if (input.Length == 8)
+                    {
+                        if (input[0] == '1' || input[0] == '2' || input[0] == '3' || input[0] == '4' || input[0] == '5' || input[0] == '6' || input[0] == '7' || input[0] == '8' || input[0] == '9')
+                        {
+                            vipGuests.Add(input);
+                        }
+                        else
+                        {
+                            regularGuests.Add(input);
+                        }
+                    }
+                    input = Console.ReadLine();
+
+                }
+                else
+                {
+                    input = Console.ReadLine();
+
+                }
+
+            }
+
+            int count = vipGuests.Count + regularGuests.Count;
+            Console.WriteLine(count);
+
+            Console.WriteLine($"{string.Join(Environment.NewLine, vipGuests)}");
+            Console.WriteLine($"{string.Join(Environment.NewLine, regularGuests)}");
+
+        }
+    }
+}
