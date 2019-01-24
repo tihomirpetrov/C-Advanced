45d610ee8ac4bc17eb879b2a0a0254ab1e61b5a9
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-24 16:44:02 +0200

    initial commit

---
+++ 03.Sets and Dictionaries Advanced - Exercise/P03.PeriodicTable/Startup.cs
@@ -0,0 +1,27 @@
+﻿namespace P03.PeriodicTable
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> chemicalElements = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                foreach (var item in input)
+                {
+                    chemicalElements.Add(item);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", chemicalElements.OrderBy(x => x)));         
+        }
+    }
+}
