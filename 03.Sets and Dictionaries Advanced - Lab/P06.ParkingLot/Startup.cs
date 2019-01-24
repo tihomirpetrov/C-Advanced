d66ab1213a61baf7a45142ed5ead9d2f8b9ba0c0
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-24 11:41:50 +0200

    initial commit

---
+++ 03.Sets and Dictionaries Advanced - Lab/P06.ParkingLot/Startup.cs
@@ -0,0 +1,43 @@
+﻿namespace P06.ParkingLot
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            string[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            HashSet<string> carNumbers = new HashSet<string>();
+
+            while (input[0] != "END")
+            {
+                string direction = input[0];
+                string carNumber = input[1];
+                if (direction == "IN")
+                {
+                    carNumbers.Add(carNumber);
+                }
+                else if (direction == "OUT")
+                {
+                    carNumbers.Remove(carNumber);
+                }
+
+                input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            }
+
+            if (carNumbers.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var item in carNumbers)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
