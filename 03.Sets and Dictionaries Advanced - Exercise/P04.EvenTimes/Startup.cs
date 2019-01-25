82c83bbb4b4520cc960b3b343bb70cf3f5f86881
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-25 10:15:19 +0200

    initial commit

--- 03.Sets and Dictionaries Advanced - Exercise/P04.EvenTimes/Startup.cs
+++ 03.Sets and Dictionaries Advanced - Exercise/P04.EvenTimes/Startup.cs
@@ -2,25 +2,30 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
 
     class Startup
     {
         public static void Main()
         {
             int n = int.Parse(Console.ReadLine());
+            List<int> sequenceNumbers = new List<int>();
             HashSet<int> numbers = new HashSet<int>();
 
             for (int i = 0; i < n; i++)
             {
                 int number = int.Parse(Console.ReadLine());
-                numbers.Add(number);
+                for (int j = 0; j < sequenceNumbers.Count; j++)
+                {
+                    if (number == sequenceNumbers[j])
+                    {
+                        numbers.Add(number);
+                    }
+                }
+                sequenceNumbers.Add(number);
+
             }
 
-            foreach (var number in numbers.Where(x => x % 2 != 0))
-            {
-                Console.WriteLine(Math.Abs(number));
-            }
+            Console.WriteLine(string.Join(" ", numbers));           
         }
     }
 }
---
+++ 03.Sets and Dictionaries Advanced - Exercise/P05.CountSymbols/Startup.cs
@@ -0,0 +1,13 @@
+﻿
+namespace P05.CountSymbols
+{
+    using System;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
