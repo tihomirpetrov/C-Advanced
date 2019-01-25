cc95e447935210174f9899b04958f70684f6d552
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-25 15:28:53 +0200

    initial commit

--- 03.Sets and Dictionaries Advanced - Exercise/P05.CountSymbols/Startup.cs
+++ 03.Sets and Dictionaries Advanced - Exercise/P05.CountSymbols/Startup.cs
@@ -2,12 +2,30 @@
 namespace P05.CountSymbols
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public class Startup
     {
         public static void Main()
         {
+            string input = Console.ReadLine();
+            Dictionary<char, int> symbolsCount = new Dictionary<char, int>();
 
+            for (int i = 0; i < input.Length; i++)
+            {
+                char symbol = input[i];
+                if (!symbolsCount.ContainsKey(symbol))
+                {
+                    symbolsCount[symbol] = 0;
+                }
+                symbolsCount[symbol]++;
+            }
+
+            foreach (var item in symbolsCount.OrderBy(x =>x.Key))
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
         }
     }
 }
