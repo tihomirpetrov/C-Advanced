897046475f5f20e573f05558f1b6b3de3587bd19
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-01-26 00:00:36 +0200

    update readme

--- 03.Sets and Dictionaries Advanced - Exercise/P07.TheV-Logger/Program.cs
+++ 03.Sets and Dictionaries Advanced - Exercise/P07.TheV-Logger/Program.cs
@@ -1,12 +1,67 @@
-﻿using System;
-
-namespace P07.TheV_Logger
+﻿namespace P07.TheV_Logger
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+            Dictionary<string, int> vloggersJoin = new Dictionary<string, int>();
+            Dictionary<string, int> vloggersFollow = new Dictionary<string, int>();
+
+            while (input[0] != "Statistics")
+            {
+                string vloggerName = input[0];
+                string action = input[1];
+
+                if (action == "joined")
+                {
+                    if (!vloggersJoin.ContainsKey(vloggerName))
+                    {
+                        vloggersJoin.Add(vloggerName, 0);
+                        vloggersJoin[vloggerName]++;
+                    }
+
+
+                }
+                else if (action == "followed")
+                {
+                    if (vloggersJoin.ContainsKey(vloggerName))
+                    {
+                        if (!vloggersFollow.ContainsKey(vloggerName))
+                        {
+                            vloggersFollow.Add(vloggerName, 0);
+                        }
+                        vloggersFollow[vloggerName]++;
+                    }
+
+                }
+
+                input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+            }
+
+            Console.WriteLine($"The V-Logger has a total of {vloggersJoin.Count} vloggers in its logs.");
+            foreach (var user in vloggersJoin)
+            {
+                Console.WriteLine($"{user.Value}. {user.Key} : {user.Value} followers, {vloggersJoin.Count} following");
+
+                foreach (var item in vloggersFollow.Keys)
+                {
+                    if (user.Key == item)
+                    {
+                        Console.WriteLine($"* {user.Key}");
+                    }
+                }
+               
+            }
+
+            //foreach (var user in vloggersFollow)
+            //{
+            //    Console.WriteLine($"{user.Key} - {user.Value}");
+            //}
         }
     }
 }
