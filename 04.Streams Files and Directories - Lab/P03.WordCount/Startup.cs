9c5694567d925ef544a4a1e1dccbe112a575f351
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-01-28 23:51:03 +0200

    initial commit

--- 04.Streams Files and Directories - Lab/P03.WordCount/Startup.cs
+++ 04.Streams Files and Directories - Lab/P03.WordCount/Startup.cs
@@ -1,11 +1,49 @@
 ﻿namespace P03.WordCount
 {
     using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
     public class Startup
     {
         public static void Main()
         {
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+            string allWords = @"..\..\..\Resources\03. Word Count\words.txt";
+            string[] words = allWords.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
 
+            using (StreamReader reader = new StreamReader(@"..\..\..\Resources\03. Word Count\text.txt"))
+            {
+                string currentLine = reader.ReadLine();
+
+                while (true)
+                {
+                    if (currentLine == null)
+                    {
+                        break;
+                    }
+
+                    for (int i = 0; i < words.Length; i++)
+                    {
+                        if (!wordCount.ContainsKey(words[i]))
+                        {
+                            wordCount.Add(words[i], 1);
+                        }
+
+                        else
+                        {
+                            wordCount[words[i]]++;
+                        }                        
+                    }
+                    currentLine = reader.ReadLine();
+                }
+
+                foreach (var item in wordCount.OrderByDescending(key => key.Value))
+                {
+                    Console.WriteLine($"{item.Key} - {item.Value}");
+                }
+            }
         }
     }
 }
