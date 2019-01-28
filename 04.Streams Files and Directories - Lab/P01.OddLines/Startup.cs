d00848f4e2a320fb89b06b1e9c88557400b253d5
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-01-28 21:33:50 +0200

    initial commit

--- 04.Streams Files and Directories - Lab/P01.OddLines/Startup.cs
+++ 04.Streams Files and Directories - Lab/P01.OddLines/Startup.cs
@@ -1,12 +1,33 @@
 ﻿namespace P01.OddLines
 {
     using System;
+    using System.IO;
 
     public class Startup
     {
         public static void Main()
         {
+            using (StreamReader reader = new StreamReader(@"..\..\..\Resources\01. Odd Lines\Input.txt"))
+            {
+                int counter = 0;
 
+                while (true)
+                {
+                    string line = reader.ReadLine();
+
+                    if (line == null)
+                    {
+                        break;
+                    }
+
+                    if (counter % 2 != 0)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                    counter++;
+                }                
+            }
         }
     }
 }
