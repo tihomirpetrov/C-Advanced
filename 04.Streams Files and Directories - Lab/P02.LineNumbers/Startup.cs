ae49b5495da58ffa4ea279af2a12b3fd82e82a97
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-01-28 21:51:27 +0200

    initial commit

---
+++ 04.Streams Files and Directories - Lab/P02.LineNumbers/Startup.cs
@@ -0,0 +1,33 @@
+﻿namespace P02.LineNumbers
+{
+    using System;
+    using System.IO;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            using (StreamReader reader = new StreamReader(@"..\..\..\Resources\02. Line Numbers\Input.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter(@"..\..\..\Resources\02. Line Numbers\Output.txt"))
+                {
+                    int counter = 1;
+
+                    while (true)
+                    {
+                        string line = reader.ReadLine();
+
+                        if (line == null)
+                        {
+                            break;
+                        }
+
+                        line = $"{counter}. {line}";
+                        counter++;
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 04.Streams Files and Directories - Lab/P03.WordCount/Startup.cs
@@ -0,0 +1,11 @@
+﻿namespace P03.WordCount
+{
+    using System;
+    public class Startup
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
