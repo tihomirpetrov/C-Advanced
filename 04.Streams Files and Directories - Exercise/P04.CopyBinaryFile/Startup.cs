72b2152892deaf883c461add20a5d5c372069c68
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-30 13:29:52 +0200

    initial commit

--- 04.Streams Files and Directories - Exercise/P04.CopyBinaryFile/Startup.cs
+++ 04.Streams Files and Directories - Exercise/P04.CopyBinaryFile/Startup.cs
@@ -1,12 +1,31 @@
 ﻿namespace P04.CopyBinaryFile
 {
     using System;
+    using System.IO;
 
     class Startup
     {
         public static void Main()
         {
-            
+            using (FileStream reader = new FileStream(@"..\..\..\copyMe.png", FileMode.Open))
+            {
+                using (FileStream writer = new FileStream(@"..\..\..\copied.png", FileMode.Create))
+                {
+                    while (true)
+                    {
+                        byte[] buffer = new byte[4096];
+
+                        int byteSize = reader.Read(buffer, 0, buffer.Length);
+
+                        if (byteSize < 1)
+                        {
+                            break;
+                        }
+
+                        writer.Write(buffer, 0, byteSize);
+                    }
+                }
+            }
         }
     }
 }
