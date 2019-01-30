0cb52b5f8606960b101bc491aa676497eec8c20a
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-30 11:32:52 +0200

    initial commit

--- 04.Streams Files and Directories - Exercise/P02.LineNumbers/Startup.cs
+++ 04.Streams Files and Directories - Exercise/P02.LineNumbers/Startup.cs
@@ -1,12 +1,43 @@
 ﻿namespace P02.LineNumbers
 {
     using System;
+    using System.IO;
 
     class Startup
     {
         public static void Main()
         {
+            using (StreamReader reader = new StreamReader(@"..\..\..\text.txt"))
+            {
+                int counter = 1;
 
+                using (StreamWriter writer = new StreamWriter(@"..\..\..\output.txt"))
+                {
+                    string line = reader.ReadLine();
+
+                    while (line != null)
+                    {
+                        int lettersCount = 0;
+                        int symbolsCount = 0;
+
+                        foreach (var @char in line)
+                        {
+                            if (char.IsLetter(@char))
+                            {
+                                lettersCount++;
+                            }
+                            else if(char.IsPunctuation(@char))
+                            {
+                                symbolsCount++;
+                            }
+                        }
+
+                        writer.WriteLine($"Line {counter}: {line} ({lettersCount})({symbolsCount})");
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
         }
     }
 }
