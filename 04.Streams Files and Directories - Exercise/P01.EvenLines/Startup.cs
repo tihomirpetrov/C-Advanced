d22bf253685385f4b45fe7fc5a3182d90906df0f
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-30 10:45:03 +0200

    initial commit

--- 04.Streams Files and Directories - Exercise/P01.EvenLines/Startup.cs
+++ 04.Streams Files and Directories - Exercise/P01.EvenLines/Startup.cs
@@ -1,12 +1,28 @@
 ﻿namespace P01.EvenLines
 {
     using System;
+    using System.IO;
 
     public class Startup
     {
         public static void Main()
         {
+            string specialSymbols = "-.,!?";
 
+            int counter = 0;
+
+            using (StreamReader reader = new StreamReader(@"..\..\..\text.txt"))
+            {
+                string line = reader.ReadLine();
+
+                using (StreamWriter writer = new StreamWriter(@"..\..\..\output.txt"))
+                {
+                    while (line != null)
+                    {
+
+                    }
+                }
+            }
         }
     }
 }
