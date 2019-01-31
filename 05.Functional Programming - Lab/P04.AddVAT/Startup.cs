0057232413e5d5a315c2edefef759c3b0c9f60cf
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-31 10:46:02 +0200

    initial commit

---
+++ 05.Functional Programming - Lab/P04.AddVAT/Startup.cs
@@ -0,0 +1,20 @@
+﻿namespace P04.AddVAT
+{
+    using System;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            Func<string, double> parser = x => double.Parse(x) * 1.2;
+
+            double[] prices = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries).Select(parser).ToArray();
+
+            foreach (var price in prices)
+            {
+                Console.WriteLine($"{price:f2}");
+            }
+        }
+    }
+}
