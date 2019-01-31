f62fac3ce26ccffcdc06e537e1692ce597da1638
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-31 08:55:21 +0200

    initial commit

---
+++ 05.Functional Programming - Lab/P02.SumNumbers/Startup.cs
@@ -0,0 +1,15 @@
+﻿namespace P02.SumNumbers
+{
+    using System;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
---
+++ 05.Functional Programming - Lab/P03.CountUppercaseWords/Startup.cs
@@ -0,0 +1,12 @@
+﻿namespace P03.CountUppercaseWords
+{
+    using System;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
