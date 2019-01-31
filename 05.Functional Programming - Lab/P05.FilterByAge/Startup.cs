6b53d3f4b327fde53a7ef437c59239543a70396a
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-31 15:01:02 +0200

    initial commit

--- 05.Functional Programming - Lab/P05.FilterByAge/Startup.cs
+++ 05.Functional Programming - Lab/P05.FilterByAge/Startup.cs
@@ -8,10 +8,36 @@
     {
         public static void Main()
         {
-            int n = int.Parse(Console.ReadLine());
+            var people = GetPeople();
+
+            string condition = Console.ReadLine();
+            int ages = int.Parse(Console.ReadLine());
+            string format = Console.ReadLine();
+
+            Func<int, bool> tester = CreateTester(condition, ages);
+            Action<KeyValuePair<string, int>> printer = CreatePrinter(format);
+
+            InvokePrinter(people, tester, printer);
+        }
+
+        private static void InvokePrinter(Dictionary<string, int> namesAges, Func<int, bool> tester, Action<KeyValuePair<string, int>> printer)
+        {
+            foreach (var person in namesAges)
+            {
+                if (tester(person.Value))
+                {
+                    printer(person);
+                }
+            }
+        }
+
+        private static Dictionary<string, int> GetPeople()
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+
             Dictionary<string, int> namesAges = new Dictionary<string, int>();
 
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < numberOfPeople; i++)
             {
                 string[] nameAge = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
                 string name = nameAge[0];
@@ -22,24 +48,32 @@
                     namesAges.Add(name, age);
                 }
             }
-
-            string condition = Console.ReadLine();
-            int ages = int.Parse(Console.ReadLine());
-            string format = Console.ReadLine();
-
-            foreach (var name in namesAges)
-            {
-                Console.WriteLine(CreateTester(name));
-            }
-
-
+            return namesAges;
         }
+
         public static Func<int, bool> CreateTester(string condition, int ages)
         {
             switch (condition)
             {
-                case "younger": return x => x < ages;
-                case "older": return x => x >= ages;
+                case "younger":
+                    return x => x < ages;
+                case "older":
+                    return x => x >= ages;
+                default:
+                    return null;
+            }
+        }
+
+        public static Action<KeyValuePair<string, int>> CreatePrinter(string format)
+        {
+            switch (format)
+            {
+                case "name":
+                    return kvp => Console.WriteLine($"{kvp.Key}");
+                case "age":
+                    return kvp => Console.WriteLine($"{kvp.Value}");
+                case "name age":
+                    return kvp => Console.WriteLine($"{kvp.Key} - {kvp.Value}");
                 default: return null;
             }
         }
