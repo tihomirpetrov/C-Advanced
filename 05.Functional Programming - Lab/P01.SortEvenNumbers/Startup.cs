ad3a1008f4810833432232717a12630006ccefd7
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-31 08:45:11 +0200

    initial commit

---
+++ 05.Functional Programming - Lab/P01.SortEvenNumbers/Startup.cs
@@ -0,0 +1,15 @@
+﻿namespace P01.SortEvenNumbers
+{
+    using System;
+    using System.Linq;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).Where(x => x % 2 == 0).OrderBy(x => x).ToArray();
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
