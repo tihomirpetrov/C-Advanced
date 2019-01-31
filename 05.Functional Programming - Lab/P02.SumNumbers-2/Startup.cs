e30b1279a15a96f1c76a378b70e6771d36819d6c
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-31 09:00:22 +0200

    initial commit

---
+++ 05.Functional Programming - Lab/P02.SumNumbers-2/Startup.cs
@@ -0,0 +1,18 @@
+﻿namespace P02.SumNumbers_2
+{
+    using System;
+    using System.Linq;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            Func<string, int> parser = x => int.Parse(x);
+            int[] numbers = input.Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(parser).ToArray();
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
