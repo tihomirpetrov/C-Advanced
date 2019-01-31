61da0dc8f759ccec50acc1d1b1a56a706280414a
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-31 16:21:29 +0200

    update readme

--- 05.Functional Programming - Exercise/P01.ActionPrint/Startup.cs
+++ 05.Functional Programming - Exercise/P01.ActionPrint/Startup.cs
@@ -1,12 +1,13 @@
 ﻿namespace P01.ActionPrint
 {
     using System;
+    using System.Linq;
 
     public class Startup
     {
         public static void Main()
         {
-            
+            Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => x).ToList().ForEach(x => Console.WriteLine(x));
         }
     }
 }
