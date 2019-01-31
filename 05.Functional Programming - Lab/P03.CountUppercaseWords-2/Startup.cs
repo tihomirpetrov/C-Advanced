ff8aa3317dba2953f555af0e0634cc1e48989236
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-31 10:29:26 +0200

    initial commit

---
+++ 05.Functional Programming - Lab/P03.CountUppercaseWords-2/Startup.cs
@@ -0,0 +1,19 @@
+﻿namespace P03.CountUppercaseWords_2
+{
+    using System;
+    using System.Linq;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            Func<string, bool> checker = x => x[0] == x.ToUpper()[0];
+            string[] words = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Where(checker).ToArray();
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
