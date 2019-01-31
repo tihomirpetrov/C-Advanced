1e88a8d1862b3ff44f724648171366888c788093
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-31 11:35:39 +0200

    initial commit

---
+++ 05.Functional Programming - Lab/P05.FilterByAge/Startup.cs
@@ -0,0 +1,47 @@
+﻿namespace P05.FilterByAge
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, int> namesAges = new Dictionary<string, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] nameAge = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string name = nameAge[0];
+                int age = int.Parse(nameAge[1]);
+
+                if (!namesAges.ContainsKey(name))
+                {
+                    namesAges.Add(name, age);
+                }
+            }
+
+            string condition = Console.ReadLine();
+            int ages = int.Parse(Console.ReadLine());
+            string format = Console.ReadLine();
+
+            foreach (var name in namesAges)
+            {
+                Console.WriteLine(CreateTester(name));
+            }
+
+
+        }
+        public static Func<int, bool> CreateTester(string condition, int ages)
+        {
+            switch (condition)
+            {
+                case "younger": return x => x < ages;
+                case "older": return x => x >= ages;
+                default: return null;
+            }
+        }
+    }
+}
