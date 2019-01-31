75316ab25e51a7fb2aacebda37f50d6101e6bdc5
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-31 16:45:27 +0200

    initial commit

--- 05.Functional Programming - Exercise/P03.CustomMinFunction/Startup.cs
+++ 05.Functional Programming - Exercise/P03.CustomMinFunction/Startup.cs
@@ -1,12 +1,17 @@
 ﻿namespace P03.CustomMinFunction
 {
     using System;
+    using System.Collections.Generic;
 
     class Startup
     {
         public static void Main()
         {
-           
+            HashSet<int> numbers = new HashSet<int>();
+
+            Console.ReadLine();
+
+            Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x <= x).ToList().ForEach(x => Console.WriteLine(x));
         }
     }
 }
