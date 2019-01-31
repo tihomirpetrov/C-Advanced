73ca337a35871cd06e28e7b240490bdf0335e895
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-01-31 10:08:51 +0200

    initial commit

--- 05.Functional Programming - Lab/P03.CountUppercaseWords/Startup.cs
+++ 05.Functional Programming - Lab/P03.CountUppercaseWords/Startup.cs
@@ -1,12 +1,20 @@
 ﻿namespace P03.CountUppercaseWords
 {
     using System;
-
+    using System.Linq;
+   
     public class Startup
     {
         public static void Main()
         {
-            
+            string input = Console.ReadLine();
+
+            var upperCaseWords = input.Split(' ').Where(x => !string.IsNullOrEmpty(x) && char.IsUpper(x[0]));          
+
+            foreach (var item in upperCaseWords)
+            {
+                Console.WriteLine(item);
+            }            
         }
     }
 }
