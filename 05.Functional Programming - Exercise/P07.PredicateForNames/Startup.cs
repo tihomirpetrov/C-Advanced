910b885286561d56fef3d14b62ef4a0e180f5f65
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-02-01 14:50:37 +0200

    initial commit

--- 05.Functional Programming - Exercise/P07.PredicateForNames/Startup.cs
+++ 05.Functional Programming - Exercise/P07.PredicateForNames/Startup.cs
@@ -6,7 +6,18 @@
     {
         public static void Main()
         {
+            int wordLength = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
+            PrintNames(names, w =>w.Length, wordLength);
+
+        }
+
+        private static void PrintNames(string[] names, Func<object, object> p, int wordLength)
+        {
+            throw new NotImplementedException();
         }
     }
 }
+
+
---
+++ 06.Defining Classes - Lab/P01.Car/Car.cs
@@ -0,0 +1,17 @@
+﻿namespace Car
+{
+    public class Car
+    {
+        private string make;
+
+        private string model;
+
+        private int year;
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+    }
+}
