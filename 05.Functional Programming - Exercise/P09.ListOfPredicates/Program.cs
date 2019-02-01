598a998f7b7e8dc2e2bc18bec36165d859d41960
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-01 19:39:56 +0200

    initial commit

---
+++ 05.Functional Programming - Exercise/P09.ListOfPredicates/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace P09.ListOfPredicates
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] divideNumbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Queue<int> numbers = new Queue<int>();
+
+            Func<int, bool>[] predicates = divideNumbers.Select(div => (Func<int, bool>)(x => x % div == 0)).ToArray();
+            
+            for (int i = 1; i <= n; i++)
+            {
+                if (isValid(predicates, i))
+                {
+                    numbers.Enqueue(i);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static bool isValid(Func<int, bool>[] predicates, int num)
+        {
+            foreach (var predicate in predicates)
+            {
+                if (!predicate(num))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
