9014e07c267d389d759a331c097a95600bf3e288
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-02-01 13:12:34 +0200

    initial commit

---
+++ 05.Functional Programming - Exercise/P06.ReverseAndExclude/Startup.cs
@@ -0,0 +1,21 @@
+﻿namespace P06.ReverseAndExclude
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse);
+            int n = int.Parse(Console.ReadLine());
+
+            Func<int, bool> filter = x => x % n != 0;
+
+            var result = numbers.Reverse().Where(filter);
+                                 
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
