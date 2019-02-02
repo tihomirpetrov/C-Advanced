a800c0a5a66b8cf958bdd5529aa42a121f6b77f5
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-02 23:01:45 +0200

    initial commit

---
+++ 05.Functional Programming - Exercise/P13.TriFunction/Startup.cs
@@ -0,0 +1,22 @@
+﻿namespace P13.TriFunction
+{
+    using System;
+    using System.Linq;
+
+    class Startup
+    {
+        public static void Main()
+        {
+            int targetCharactersSum = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+
+            Func<string, int, bool> isValidWord = (str, num) => str.ToCharArray()
+                .Select(ch => (int)ch).Sum() >= num;
+
+            Func<string[], int, Func<string, int, bool>, string> firstName = (arr, num, func) => arr.FirstOrDefault(str => func(str, num));
+
+            string result = firstName(names, targetCharactersSum, isValidWord);
+            Console.WriteLine(result);
+        }
+    }
+}
