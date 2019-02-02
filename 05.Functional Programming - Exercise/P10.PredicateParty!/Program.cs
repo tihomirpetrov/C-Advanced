40ff42e190e90d3a91f9eec0c19d59b432ca3669
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-02 21:52:36 +0200

    initial commit

---
+++ 05.Functional Programming - Exercise/P10.PredicateParty!/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace P10.PredicateParty
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            List<string> commingPeople = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+
+        }
+    }
+}
