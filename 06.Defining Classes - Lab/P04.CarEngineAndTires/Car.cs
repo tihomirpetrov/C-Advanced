6cec92ae026861db309cbebb27aa2ed6a5c22eb6
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-03 16:05:45 +0200

    initial commit

---
+++ 06.Defining Classes - Lab/P04.CarEngineAndTires/Car.cs
@@ -0,0 +1,116 @@
+﻿namespace CarManufacturer
+{
+    using System;
+    using System.Text;
+
+    public class Car
+    {
+        private string make;
+
+        private string model;
+
+        private int year;
+
+        private double fuelQuantity;
+
+        private double fuelConsumption;
+
+        private Engine engine;
+
+        private Tire[] tires;
+
+        public string Make
+        {
+            get { return this.make; }
+            set { this.make = value; }
+        }
+
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+
+        public int Year
+        {
+            get { return this.year; }
+            set { this.year = value; }
+        }
+
+        public double FuelQuantity
+        {
+            get { return this.fuelQuantity; }
+            set { this.fuelQuantity = value; }
+        }
+
+        public double FuelConsumption
+        {
+            get { return this.fuelConsumption; }
+            set { this.fuelConsumption = value; }
+        }
+
+        public Engine Engine { get; set; }
+
+        public Tire[] Tires { get; set; }
+
+        public Car()
+        {
+            this.Make = "VW";
+            this.Model = "Golf";
+            this.Year = 2025;
+            this.FuelQuantity = 200;
+            this.FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year)
+            : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+            : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires)
+            : this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            this.Engine = engine;
+            this.Tires = tires;
+        }
+
+        public void Drive(double distance)
+        {
+            double consumedFuel = distance * (this.fuelConsumption / 100);
+
+            if (this.fuelQuantity - consumedFuel < 0)
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+            else
+            {
+                this.fuelQuantity -= consumedFuel;
+            }
+        }
+
+        public string WhoAmI()
+        {
+            StringBuilder carInfo = new StringBuilder();
+
+            carInfo.AppendLine($"Make: {this.Make}");
+
+            carInfo.AppendLine($"Model: {this.Model}");
+
+            carInfo.AppendLine($"Year: {this.Year}");
+
+            carInfo.Append($"Fuel: {this.FuelQuantity:F2}L");
+
+            return carInfo.ToString();
+        }
+    }
+}
