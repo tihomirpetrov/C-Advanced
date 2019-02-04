1d1eb45bddf1e80c57d58aa3af7715c2371d2fb3
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-04 20:59:08 +0200

    initial commit

---
+++ 06.Defining Classes - Exercise/P03.OldestFamilyMember/Family.cs
@@ -0,0 +1,21 @@
+﻿namespace DefiningClasses
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    public class Family
+    {
+        private List<Person> people = new List<Person>();
+
+        public void AddMember(Person member)
+        {
+            people.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldestMember = people.OrderByDescending(x => x.Age).FirstOrDefault();
+
+            return oldestMember;
+        }
+    }
+}
---
+++ 06.Defining Classes - Exercise/P03.OldestFamilyMember/Startup.cs
@@ -0,0 +1,27 @@
+﻿namespace DefiningClasses
+{
+    using System;
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Family members = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                Person currentPerson = new Person(name, age);
+
+                members.AddMember(currentPerson);
+            }
+
+            Person oldestPerson = members.GetOldestMember();
+
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+        }
+    }
+}
