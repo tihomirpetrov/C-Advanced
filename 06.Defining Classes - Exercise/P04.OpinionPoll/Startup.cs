4c2cd91ea9678f839f0a39feccce0a02bcb85b0a
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-04 21:08:22 +0200

    initial commit

---
+++ 06.Defining Classes - Exercise/P04.OpinionPoll/Startup.cs
@@ -0,0 +1,32 @@
+﻿namespace DefiningClasses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                Person currentPerson = new Person(name, age);
+
+                people.Add(currentPerson);
+            }
+
+            foreach (var person in people.Where(x => x.Age > 30).OrderBy(x => x.Name))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+
+        }
+    }
+}
