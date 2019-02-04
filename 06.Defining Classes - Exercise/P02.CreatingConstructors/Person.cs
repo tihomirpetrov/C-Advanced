47dad52ec64b988970a2773a4e52c3e3d9c0a838
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-02-04 13:47:16 +0200

    initial commit

---
+++ 06.Defining Classes - Exercise/P02.CreatingConstructors/Person.cs
@@ -0,0 +1,43 @@
+﻿namespace DefiningClasses
+{
+    using System;
+
+    public class Person
+    {
+        private string name;
+
+        private int age;
+
+        public Person()
+        {
+            this.Name = "No name";
+            this.Age = 1;
+        }
+
+        public Person(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new NullReferenceException("Invalid name");
+            }
+
+            this.name = name;
+        }
+
+        public Person(string name, int age) : this(name)
+        {
+            this.age = age;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+    }
+}
