e6c5bacc3502f51e77c3b0a7942b955642384aab
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-05 19:19:38 +0200

    initial commit

---
+++ 06.Defining Classes - Exercise/P05.DateModifier/DateModifier.cs
@@ -0,0 +1,34 @@
+﻿namespace P05.DateModifier
+{
+    using System;
+    using System.Globalization;
+
+    public class DateModifier
+    {
+        private DateTime startDate;
+        private DateTime endDate;
+
+        public DateModifier(DateTime startDateStr, DateTime endDateStr)
+        {
+            this.startDate = startDateStr;
+            this.endDate = endDateStr;            
+        }
+
+        public DateTime StartDate
+        {
+            get { return this.startDate; }
+            set { this.startDate = value; }
+        }
+
+        public DateTime EndDate
+        {
+            get { return this.endDate; }
+            set { this.endDate = value; }
+        }
+
+        public double CalculateDifferenceInDays()
+        {
+            return Math.Abs((EndDate - StartDate).TotalDays);
+        }
+    }
+}
---
+++ 06.Defining Classes - Exercise/P05.DateModifier/Startup.cs
@@ -0,0 +1,20 @@
+﻿namespace P05.DateModifier
+{
+    using System;
+    using System.Globalization;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string startDateStr = Console.ReadLine();
+            string endDateStr = Console.ReadLine();
+            DateTime startDate = DateTime.Parse(startDateStr, CultureInfo.InvariantCulture);
+            DateTime endDate = DateTime.Parse(endDateStr, CultureInfo.InvariantCulture);
+
+            DateModifier dateModifier = new DateModifier(startDate, endDate);
+
+            Console.WriteLine(dateModifier.CalculateDifferenceInDays());
+        }       
+    }
+}
