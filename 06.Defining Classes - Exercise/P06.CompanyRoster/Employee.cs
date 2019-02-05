3041f64bdebeec1610e9cceb0ce8b39de5ccfa51
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-05 20:28:20 +0200

    initial commit

---
+++ 06.Defining Classes - Exercise/P06.CompanyRoster/Employee.cs
@@ -0,0 +1,74 @@
+﻿namespace P06.CompanyRoster
+{
+    using System;
+    using System.Collections.Generic;
+    public class Employee
+    {
+        private string name;
+
+        private double salary;
+
+        private string position;
+
+        private string department;
+
+        private string email;
+
+        private int age;
+
+        public Employee(string name, double salary, string position, string department)
+        {
+            this.Name = name;
+            this.Salary = salary;
+            this.Position = position;
+            this.Department = department;
+            this.Email = "n/a";
+            this.Age = -1;
+        }
+
+        public Employee(string name, double salary, string position, string department, string email) : this(name, salary, position, department)
+        {
+            this.Email = email;
+        }
+
+        public Employee(string name, double salary, string position, string department, int age) : this(name, salary, position, department)
+        {
+            this.Age = age;
+        }
+
+        public Employee(string name, double salary, string position, string department,string email, int age) : this(name, salary, position, department)
+        {
+            this.Email = email;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public double Salary
+        {
+            get { return this.salary; }
+            set { this.salary = value; }
+        }
+
+        public string Position
+        {
+            get { return this.position; }
+            set { this.position = value; }
+        }
+
+        public string Department
+        {
+            get { return this.department; }
+            set { this.department = value; }
+        }
+
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+
+    }
+}
---
+++ 06.Defining Classes - Exercise/P06.CompanyRoster/Startup.cs
@@ -0,0 +1,68 @@
+﻿namespace P06.CompanyRoster
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Employee> employees = new List<Employee>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string name = input[0];
+                double salary = double.Parse(input[1]);
+                string position = input[2];
+                string department = input[3];               
+
+                if (input.Length == 4)
+                {
+                    Employee currentEmployee = new Employee(name, salary, position, department);
+                    employees.Add(currentEmployee);
+                }
+
+                if (input.Length == 6)
+                {
+                    string email = input[4];
+                    int age = int.Parse(input[5]);
+
+                    Employee currentEmployee = new Employee(name, salary, position, department, email, age);
+                    employees.Add(currentEmployee);
+                }
+
+                if (input.Length == 5)
+                {
+                    int num;
+                    bool isNumeric = int.TryParse(input[4], out num);
+
+                    if (isNumeric)
+                    {
+                        int age = int.Parse(input[4]);
+
+                        Employee currentEmployee = new Employee(name, salary, position, department, age);
+                        employees.Add(currentEmployee);
+                    }
+                    else
+                    {
+                        string email = input[4];
+                        Employee currentEmployee = new Employee(name, salary, position, department, email);
+                        employees.Add(currentEmployee);
+                    }
+                }
+            }
+
+            string highestSalaryDepartment = employees.GroupBy(x => x.Department).OrderByDescending(x => x.Average(s => s.Salary)).FirstOrDefault().Key;
+
+            Console.WriteLine($"Highest Average Salary: {highestSalaryDepartment}");
+
+            foreach (Employee employee in employees.Where(x => x.Department == highestSalaryDepartment).OrderByDescending(x => x.Salary))
+            {
+                Console.WriteLine($"{employee.Name} {employee.Salary:f2} {employee.Email} {employee.Age}");
+            }
+        }
+    }
+}
