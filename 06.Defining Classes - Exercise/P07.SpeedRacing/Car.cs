9c6628f14b586119174a27b65bb69550382b0e96
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-05 20:58:05 +0200

    initial commit

---
+++ 06.Defining Classes - Exercise/P07.SpeedRacing/Car.cs
@@ -0,0 +1,29 @@
+﻿namespace P07.SpeedRacing
+{
+    class Car
+    {
+        private string model;
+
+        private int fuelAmount;
+
+        private double fuelConsumptionPerKm;
+
+        private double traveledDistance;
+
+        public Car(string model, int fuelAmount, double fuelConsumptionPerKm)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumptionPerKm = fuelConsumptionPerKm;
+        }
+
+
+        public string Model { get; set; }
+
+        public int FuelAmount { get; set; }
+
+        public double FuelConsumptionPerKm { get; set; }
+
+        public double TraveledDistance { get; set; }
+    }
+}
---
+++ 06.Defining Classes - Exercise/P07.SpeedRacing/Startup.cs
@@ -0,0 +1,37 @@
+﻿namespace P07.SpeedRacing
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int numberOfCars = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+            string[] input;
+
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                input = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+                string model = input[0];
+                int fuelAmount = int.Parse(input[1]);
+                double fuelConsumption = double.Parse(input[2]);
+                Car currentCar = new Car(model, fuelAmount, fuelConsumption);
+
+                cars.Add(currentCar);
+            }
+
+            input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (input[0] != "End")
+            {
+                string model = input[1];
+                double traveledDistance = double.Parse(input[2]);
+
+                input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+    }
+}
