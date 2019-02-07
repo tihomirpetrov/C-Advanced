ca22b122aad9c7cf5a487feb0ec65de7e10e3838
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-02-07 08:58:33 +0200

    initial commit

---
+++ 06.Defining Classes - Exercise/P11.PokemonTrainer/Startup.cs
@@ -0,0 +1,12 @@
+﻿namespace P11.PokemonTrainer
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
---
+++ 06.Defining Classes - Exercise/P11.PokemonTrainer/Trainer.cs
@@ -0,0 +1,18 @@
+﻿namespace P11.PokemonTrainer
+{
+    public class Trainer
+    {
+        public Trainer(string name, int numberOfBadges, int collectionOfPokemon)
+        {
+            this.Name = name;
+            this.NumberOfBadges = numberOfBadges;
+            this.CollectionOfPokemon = collectionOfPokemon;
+        }
+
+        public string Name { get; set; }
+
+        public int NumberOfBadges { get; set; }
+
+        public int CollectionOfPokemon { get; set; }
+    }
+}
