509b102ebf4e9f94b123a38d526c8565d24e53bc
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-02-08 11:06:07 +0200

    initial commit

---
+++ ExamPreparation/P01.ReverseStrings/Startup.cs
@@ -0,0 +1,23 @@
+﻿namespace P01.ReverseStrings
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> stackOfChars = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char singleChar = input[i];
+                stackOfChars.Push(singleChar);
+            }
+
+            Console.WriteLine(string.Join("",stackOfChars));
+        }
+    }
+}
