7e8e09a2673634765f97a6e2b5d29520d5477a0a
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-02-08 16:51:38 +0200

    initial commit

---
+++ ExamPreparation/P03.SimpleCalculator/Startup.cs
@@ -0,0 +1,33 @@
+﻿namespace P03.SimpleCalculator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            Stack<string> elements = new Stack<string>(Console.ReadLine().Split().ToArray());
+            Stack<int> numbers = new Stack<int>();
+            elements.Reverse();
+
+            foreach (var element in elements)
+            {
+                if (element == "+")
+                {
+                    numbers.Push(int.Parse(element));
+                }
+                else if (element == "-")
+                {
+                    //numbers.Pop(int.Parse(element));
+                }
+                else
+                {
+                    numbers.Push(int.Parse(element));
+                    elements.Pop();
+                }
+            }
+        }
+    }
+}
