04952ecd072147938b641a1e1e4d8d9495b31c26
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-02-08 10:52:57 +0200

    initial commit

---
+++ 08.Generics-Lab/P01.Box/Box.cs
@@ -0,0 +1,38 @@
+﻿namespace BoxOfT
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Box<T>
+    {
+        public List<T> CreateList<T>(T item, int count)
+        {
+            List<T> list = new List<T>();
+
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(item);
+            }
+
+            return list;
+        }
+        
+        private int count;
+        private T[] data;
+                
+        public int Count { get; set; }
+       
+        public void Add(T item)
+        {
+            this.data.Add(item);
+        }
+
+        public T Remove()
+        {
+            var rem = this.data.Last();
+            this.data.RemoveAt(this.data.Count - 1);
+            return num;
+        }
+    }
+}
