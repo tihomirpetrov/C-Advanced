e6f6fb71f6ecac4d53b3e91ece7aac1a3a65cfe2
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-02-08 13:50:20 +0200

    initial commit

---
+++ ExamPreparation/P02.StackSum/Startup.cs
@@ -0,0 +1,43 @@
+﻿namespace P02.StackSum
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            Stack<int> stackOfNumbers = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            string input = Console.ReadLine().ToLower();
+
+            while (input != "end")
+            {
+                string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (tokens[0] == "add")
+                {
+                    stackOfNumbers.Push(int.Parse(tokens[1]));
+                    stackOfNumbers.Push(int.Parse(tokens[2]));
+                }
+
+                else if (tokens[0] == "remove")
+                {
+                    int index = int.Parse(tokens[1]);
+                    if (stackOfNumbers.Count > index)
+                    {
+                        for (int i = 0; i < index; i++)
+                        {
+                            stackOfNumbers.Pop();                            
+                        }
+                    }
+                }
+
+                input = Console.ReadLine().ToLower();
+            }       
+
+            Console.WriteLine($"Sum: {stackOfNumbers.Sum()}");
+        }
+    }
+}
