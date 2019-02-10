93292e5c724c55c4bada0ef4dca380bae8aee658
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-10 13:00:15 +0200

    initial commit

---
+++ 08.Generics-Exercise/P02.GenericBoxOfInteger/Startup.cs
@@ -0,0 +1,18 @@
+﻿namespace P02.GenericBoxOfInteger
+{
+    using System;
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int content = int.Parse(Console.ReadLine());
+                Box<int> boxWithInt = new Box<int>(content);
+                Console.WriteLine(boxWithInt.ToString());
+            }
+        }
+    }
+}
