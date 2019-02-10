4fb6474b3f86f4f911f70e35a84d1d21bbdb7d58
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-10 12:48:59 +0200

    initial commit

---
+++ 08.Generics-Exercise/P01.GenericBoxOfString/Box.cs
@@ -0,0 +1,17 @@
+﻿namespace P01.GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private T item;
+
+        public Box(T item)
+        {
+            this.item = item;
+        }
+
+        public override string ToString()
+        {
+            return $"{item.GetType()}: {item}";
+        }
+    }
+}
---
+++ 08.Generics-Exercise/P01.GenericBoxOfString/Startup.cs
@@ -0,0 +1,18 @@
+﻿namespace P01.GenericBoxOfString
+{
+    using System;
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string content = Console.ReadLine();
+                Box<string> boxWithString = new Box<string>(content);
+                Console.WriteLine(boxWithString.ToString());
+            }
+        }
+    }
+}
