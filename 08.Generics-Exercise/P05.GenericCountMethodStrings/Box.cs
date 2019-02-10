8a9d8d50920519b391e7257d9429370ace9d8017
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-10 14:49:58 +0200

    initial commit

---
+++ 08.Generics-Exercise/P05.GenericCountMethodStrings/Box.cs
@@ -0,0 +1,33 @@
+﻿namespace P05.GenericCountMethodStrings
+{
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Box<T>
+    {
+        private List<T> data;
+
+        public Box()
+        {
+            this.data = new List<T>();
+        }
+        public List<T> Data => this.data;
+
+        public void Add(T item)
+        {
+            this.data.Add(item);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in this.data)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 08.Generics-Exercise/P05.GenericCountMethodStrings/Startup.cs
@@ -0,0 +1,48 @@
+﻿namespace P05.GenericCountMethodStrings
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string line = Console.ReadLine();
+                box.Add(line);
+            }
+
+            string element = Console.ReadLine();
+
+            int count = GetCountOfGreaterElements<string>(box.Data, element);
+
+            Console.WriteLine(count);
+        }
+        public static int GetCountOfGreaterElements<T>(List<T> listWithData, T element)
+            where T : IComparable
+        {
+            int count = 0;
+
+            foreach (var item in listWithData)
+            {
+                if (item.CompareTo(element) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        private static void Swap<T>(List<T> listWithData, int firstIndex, int secondIndex)
+        {
+            var temp = listWithData[firstIndex];
+            listWithData[firstIndex] = listWithData[secondIndex];
+            listWithData[secondIndex] = temp;
+        }
+    }
+}
