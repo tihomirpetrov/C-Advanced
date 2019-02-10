136f55499219b8fea5eb42c7a91ede153339a3a6
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-10 13:39:34 +0200

    initial commit

---
+++ 08.Generics-Exercise/P03.GenericSwapMethodString/Startup.cs
@@ -0,0 +1,37 @@
+﻿namespace P03.GenericSwapMethodString
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string line = Console.ReadLine();
+                box.Add(line);                
+            }
+
+            int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int firstIndex = indexes[0];
+            int secondIndex = indexes[1];
+
+            Swap(box.Data, firstIndex, secondIndex);
+
+            Console.WriteLine(box);
+        }
+
+        public static void Swap<T>(List<T> listWithData, int firstIndex, int secondIndex)
+        {
+            T temp = listWithData[firstIndex];
+            listWithData[firstIndex] = listWithData[secondIndex];
+            listWithData[secondIndex] = temp;
+        }
+    }
+}
