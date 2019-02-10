3182b35a782bd08fe1e063fc49bca838acf30dee
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-10 14:16:28 +0200

    initial commit

---
+++ 08.Generics-Exercise/P04.GenericSwapMethodIntegers/Box.cs
@@ -0,0 +1,34 @@
+﻿namespace P04.GenericSwapMethodIntegers
+{
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Box<T>
+    {
+        private List<T> data;
+
+        public Box()
+        {
+            this.data = new List<T>();
+        }
+
+        public List<T> Data => this.data;
+
+        public void Add(T item)
+        {
+            this.data.Add(item);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in this.data)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 08.Generics-Exercise/P04.GenericSwapMethodIntegers/Startup.cs
@@ -0,0 +1,36 @@
+﻿namespace P04.GenericSwapMethodIntegers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Box<int> box = new Box<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int line = int.Parse(Console.ReadLine());
+                box.Add(line);
+            }
+
+            int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int firstIndex = indexes[0];
+            int secondIndex = indexes[1];
+
+            Swap(box.Data, firstIndex, secondIndex);
+
+            Console.WriteLine(box);
+        }
+
+        private static void Swap<T>(List<T> listWithData, int firstIndex, int secondIndex)
+        {
+            T temp = listWithData[firstIndex];
+            listWithData[firstIndex] = listWithData[secondIndex];
+            listWithData[secondIndex] = temp;
+        }
+    }
+}
