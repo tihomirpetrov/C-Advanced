83510a1c7eb9be184f7fe449337b35f0b267b3e1
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-10 15:47:38 +0200

    initial commit

---
+++ 08.Generics-Exercise/P07.Tuple/Startup.cs
@@ -0,0 +1,33 @@
+﻿namespace P07.Tuple
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] firstInput = Console.ReadLine().Split();
+            string name = firstInput[0] + " " + firstInput[1];
+            string address = firstInput[2];
+
+            Tuple<string, string> firstTuple = new Tuple<string, string>(name, address);
+
+            string[] secondInput = Console.ReadLine().Split();
+            string secondName = secondInput[0];
+            int liters = int.Parse(secondInput[1]);
+
+            Tuple<string, int> secondTuple = new Tuple<string, int>(secondName, liters);
+
+            string[] thirdInput = Console.ReadLine().Split();
+            int intNumber = int.Parse(thirdInput[0]);
+            double doubleNumber = double.Parse(thirdInput[1]);
+
+            Tuple<int, double> thirdTuple = new Tuple<int, double>(intNumber, doubleNumber);
+
+            Console.WriteLine(firstTuple);
+            Console.WriteLine(secondTuple);
+            Console.WriteLine(thirdTuple);
+        }
+    }
+}
