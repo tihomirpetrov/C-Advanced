8746f3a550256dac44ae5609bb9e3d0ad7856e68
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-02-13 14:53:17 +0200

    initial commit

---
+++ 09.Iterators and Comparators - Lab/P02.LibraryIterator/Book.cs
@@ -0,0 +1,29 @@
+﻿namespace IteratorsAndComparators
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Book
+    {
+        private List<string> authors;
+
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.authors = new List<string>(authors);
+        }
+
+        public string Title { get; private set; }
+
+        public int Year { get; private set; }
+
+        public IReadOnlyCollection<string> Authors
+        {
+            get
+            {
+                return this.authors.AsReadOnly();
+            }
+        }
+    }
+}
