d0b96ba666ef238a44ea025ddfdecfd9c821a5fc
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-02-15 15:31:35 +0200

    initial commit

--- 09.Iterators and Comparators - Exercise/P01.ListyIterator/Startup.cs
+++ 09.Iterators and Comparators - Exercise/P01.ListyIterator/Startup.cs
@@ -1,12 +1,42 @@
 ﻿namespace P01.ListyIterator
 {
     using System;
+    using System.Linq;
 
     public class StartUp
     {
         public static void Main()
         {
-            
+            string input = Console.ReadLine();
+            ListyIterator<string> listyIterator = null;
+
+            while (input != "END")
+            {
+                string[] splittedInput = input.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string command = splittedInput[0];
+
+                if (command == "Create")
+                {
+                    listyIterator = new ListyIterator<string>(splittedInput.Skip(1).ToList());
+                }
+
+                else if (command == "Move")
+                {
+
+                }
+
+                else if (command == "Print")
+                {
+
+                }
+
+                else if (command == "HasNext")
+                {
+
+                }
+
+                input = Console.ReadLine();
+            }
         }
     }
 }
