a387a3c939297162630e9b83295ba69118b389fa
Tihomir Petrov <38465964+tihomirpetrov@users.noreply.github.com>
2019-02-15 15:21:21 +0200

    initial commit

--- 09.Iterators and Comparators - Exercise/P01.ListyIterator/ListyIterator.cs
+++ 09.Iterators and Comparators - Exercise/P01.ListyIterator/ListyIterator.cs
@@ -1,6 +1,35 @@
 ﻿namespace P01.ListyIterator
 {
+    using System.Collections.Generic;
+
     public class ListyIterator<T>
     {
+        private List<T> data;
+        private int index;
+
+        public ListyIterator(List<T> data)
+        {
+            this.data = data;
+            int index = 0;
+        }
+
+        public bool Move()
+        {
+
+        }
+
+        public bool HasNext()
+        {
+            if (index + 1 < this.data.Count)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string Print()
+        {
+
+        }
     }
 }
