5ad9be479c497eec6ea764eecf7d9921953593c8
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-17 00:17:58 +0200

    initial commit

---
+++ 09.Iterators and Comparators - Exercise/P03.Stack/CustomStack.cs
@@ -0,0 +1,45 @@
+﻿namespace P03.Stack
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        private List<T> data;
+
+        public CustomStack(params T[] data)
+        {
+            this.data = data.ToList();
+        }
+
+        public void Push(params T[] itemsToAdd)
+        {
+            this.data.AddRange(itemsToAdd);
+        }
+
+        public T Pop()
+        {
+            if (this.data.Count == 0)
+            {
+                throw new ArgumentException("No elements");
+            }
+            T element = this.data[this.data.Count - 1];
+            this.data.Remove(element);
+
+            return element;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.data.Count - 1; i >= 0; i--)
+            {
+                yield return this.data[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        => this.GetEnumerator();
+    }
+}
---
+++ 09.Iterators and Comparators - Exercise/P03.Stack/Startup.cs
@@ -0,0 +1,48 @@
+﻿namespace P03.Stack
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            CustomStack<int> stack = new CustomStack<int>();
+
+            while (input != "END")
+            {
+                string[] splittedInput = input.Split(" ", 2).ToArray();
+                string command = splittedInput[0];
+
+                if (command == "Push")
+                {
+                    int[] numbers = splittedInput[1].Split(", ").Select(int.Parse).ToArray();
+                    stack.Push(numbers);
+                }
+                else
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (Exception ex)
+                    {
+
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (var number in stack)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}
