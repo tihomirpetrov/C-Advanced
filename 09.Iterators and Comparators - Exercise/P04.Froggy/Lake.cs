f6cd24b9e6d792317c005332d2fdb5300462025c
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-17 00:44:08 +0200

    initial commit

---
+++ 09.Iterators and Comparators - Exercise/P04.Froggy/Lake.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P04.Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private List<int> stoneValues;
+
+        public Lake(params int[] stoneValues)
+        {
+            this.stoneValues = stoneValues.ToList();
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < this.stoneValues.Count; i += 2)
+            {
+                yield return this.stoneValues[i];
+            }
+
+            int startReversedIndex = this.stoneValues.Count - 1 % 2 != 0 ? this.stoneValues.Count - 2 : this.stoneValues.Count - 1;
+
+            for (int i = startReversedIndex; i >= 0; i -= 2)
+            {
+                yield return this.stoneValues[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        => this.GetEnumerator();
+    }
+}
---
+++ 09.Iterators and Comparators - Exercise/P04.Froggy/Startup.cs
@@ -0,0 +1,17 @@
+﻿namespace P04.Froggy
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int[] stoneValues = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Lake lake = new Lake(stoneValues);
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
