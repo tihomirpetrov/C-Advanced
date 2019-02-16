2be44ee4587231ec6eb250f1ff946cd4839c8156
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-17 01:39:08 +0200

    initial commit

---
+++ 09.Iterators and Comparators - Exercise/P06.StrategyPattern/PersonNameLengthComparer.cs
@@ -0,0 +1,17 @@
+﻿
+namespace P06.StrategyPattern
+{
+    using System.Collections.Generic;
+    public class PersonNameLengthComparer : IComparer<Person>
+    {
+        public int Compare(Person first, Person second)
+        {
+            int nameLengthResult = first.Name.Length.CompareTo(second.Name.Length);
+
+            if (nameLengthResult == 0)
+            {
+                return first.Name.ToLower()[0].CompareTo(second.Name.ToLower()[0]);
+            }
+        }
+    }
+}
