bf14ee7ddc8209afd594c64295c7701d2adead8e
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-17 00:57:46 +0200

    initial commit

--- 09.Iterators and Comparators - Exercise/P05.ComparingObjects/Person.cs
+++ 09.Iterators and Comparators - Exercise/P05.ComparingObjects/Person.cs
@@ -1,7 +1,39 @@
 ﻿namespace P05.ComparingObjects
 {
-    public class Person
+    public class Person : Icomparable<Person>
     {
+        private string name;
+        private int age;
+        private string town;
 
+        public Person(string name, int age, string town)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+        }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Town { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            int resultNames = this.Name.CompareTo(other.Name);
+
+            if (resultNames == 0)
+            {
+                int resultAge = this.Age.CompareTo(other.Age);
+
+                if (resultAge == 0)
+                {
+                    return this.Town.CompareTo(other.Town);
+                }
+                return resultAge;
+            }
+
+            return resultNames;
+        }
     }
 }
