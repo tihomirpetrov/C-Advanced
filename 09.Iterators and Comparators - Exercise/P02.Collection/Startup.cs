60120b47031281c3dad20a08cf6fb77a8db2839f
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-16 23:26:56 +0200

    initial commit

--- 09.Iterators and Comparators - Exercise/P02.Collection/Startup.cs
+++ 09.Iterators and Comparators - Exercise/P02.Collection/Startup.cs
@@ -1,11 +1,50 @@
 ﻿namespace P02.Collection
 {
     using System;
+    using System.Linq;
+
     public class StartUp
     {
         public static void Main()
         {
-            
+            string input = Console.ReadLine();
+            ListyIterator<string> listyiterator = null;
+
+            while (input != "END")
+            {
+                string[] splittedInput = input.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string command = splittedInput[0];
+
+                if (command == "Create")
+                {
+                    listyiterator = new ListyIterator<string>(splittedInput.Skip(1).ToList());
+                }
+
+                else if (command == "Move")
+                {
+                    Console.WriteLine(listyiterator.Move());
+                }
+
+                else if (command == "Print")
+                {
+                    Console.WriteLine(listyiterator.Print());
+                }
+
+                else if (command == "HasNext")
+                {
+                    Console.WriteLine(listyiterator.HasNext());
+                }
+
+                else if (command == "PrintAll")
+                {
+                    foreach (var element in listyiterator)
+                    {
+                        Console.Write(element + " ");
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
         }
     }
 }
