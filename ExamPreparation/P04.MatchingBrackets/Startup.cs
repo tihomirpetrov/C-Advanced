9b9494410b5a8933a33ad5a161300e910f3ddfc6
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-18 12:17:51 +0200

    initial commit

---
+++ ExamPreparation/P04.MatchingBrackets/Startup.cs
@@ -0,0 +1,27 @@
+﻿namespace P04.MatchingBrackets
+{
+    using System;
+    using System.Collections.Generic;
+    public class Startup
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            Stack<int> expressions = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    expressions.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    int startIndex = expressions.Pop();
+                    string reminder = input.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(reminder);
+                }
+            }
+        }
+    }
+}
---
+++ ExamPreparation/P05.Supermarket/Startup.cs
@@ -0,0 +1,33 @@
+﻿namespace P05.Supermarket
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            Queue<string> customers = new Queue<string>();
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    while (customers.Count > 0)
+                    {
+                        Console.WriteLine(customers.Dequeue());
+                    }
+                }
+                else
+                {
+                    string customer = input;
+                    customers.Enqueue(customer);
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{customers.Count} people remaining.");
+        }
+    }
+}
