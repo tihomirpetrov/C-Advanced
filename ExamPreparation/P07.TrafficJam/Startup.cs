a64e8c16450aff021a8d50247cb9fedd7b27c24d
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-18 13:24:27 +0200

    initial commit

--- ExamPreparation/P07.TrafficJam/Startup.cs
+++ ExamPreparation/P07.TrafficJam/Startup.cs
@@ -1,11 +1,42 @@
 ﻿namespace P07.TrafficJam
 {
     using System;
+    using System.Collections.Generic;
+
     public class Startup
     {
         public static void Main()
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> vehicles = new Queue<string>();
+            int count = 0;
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+
+                if (command != "green")
+                {
+                    string vehicle = command;
+                    vehicles.Enqueue(vehicle);
+                }
+                else if (command == "green")
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (vehicles.Count == 0)
+                        {
+                            break;
+                        }
+                        Console.WriteLine($"{vehicles.Dequeue()} passed!");
+                        count++;
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{count} cars passed the crossroads.");
         }
     }
 }
