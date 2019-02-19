4b212746e983fb258332745ed9f30798f65ea4a5
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-19 21:33:29 +0200

    initial commit

--- ExamPreparation/P02.ExcelFunctions-/Startup.cs
+++ ExamPreparation/P02.ExcelFunctions-/Startup.cs
@@ -9,13 +9,13 @@
         public static void Main()
         {
             int headerRows = int.Parse(Console.ReadLine());
-            string[][] jaggedMatrix = new string[headerRows][];
+            string[][] table = new string[headerRows][];
 
             for (int i = 0; i < headerRows; i++)
             {
                 string[] rowValues = Console.ReadLine().Split(", ");
 
-                jaggedMatrix[i] = rowValues;
+                table[i] = rowValues;
             }
 
             string[] commandArgs = Console.ReadLine().Split();
@@ -24,9 +24,16 @@
 
             if (command == "hide")
             {
-
+                int headerIndex = Array.IndexOf(table[0], header);
+                
+                for (int row = 0; row < table.Length; row++)
+                {
+                    List<string> lineToPrint = new List<string>();
+                    lineToPrint.AddRange(table[row].Take(headerIndex).ToList());
+                    lineToPrint.AddRange(table[row].Skip(headerIndex + 1));
+                    Console.WriteLine(string.Join(" | ", lineToPrint));
+                }
             }
-
         }
     }
 }
