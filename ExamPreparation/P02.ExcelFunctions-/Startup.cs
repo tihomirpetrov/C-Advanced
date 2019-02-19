884ea1859628d36449b8fb62e35ab4e8024d7489
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-19 20:33:49 +0200

    initial commit

---
+++ ExamPreparation/P02.ExcelFunctions-/Startup.cs
@@ -0,0 +1,32 @@
+﻿namespace P02.ExcelFunctions_
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            int headerRows = int.Parse(Console.ReadLine());
+            string[][] jaggedMatrix = new string[headerRows][];
+
+            for (int i = 0; i < headerRows; i++)
+            {
+                string[] rowValues = Console.ReadLine().Split(", ");
+
+                jaggedMatrix[i] = rowValues;
+            }
+
+            string[] commandArgs = Console.ReadLine().Split();
+            string command = commandArgs[0];
+            string header = commandArgs[1];
+
+            if (command == "hide")
+            {
+
+            }
+
+        }
+    }
+}
---
+++ ExamPreparation/P04.FastFood/Startup.cs
@@ -0,0 +1,45 @@
+﻿namespace P04.FastFood
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            int quantityOfFood = int.Parse(Console.ReadLine());
+            int[] quantityOfOrders = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> orders = new Queue<int>();
+            int count = 0;
+
+            for (int i = 0; i < quantityOfOrders.Length; i++)
+            {
+                if (quantityOfFood >= orders.Sum())
+                {
+                    orders.Enqueue(quantityOfOrders[i]);
+                    quantityOfFood -= quantityOfOrders[i];
+                    count++;
+                }
+                else
+                {
+                    Console.Write("Orders left: ");
+
+                    for (int j = quantityOfOrders.Length - 1; j >= count; j--)
+                    {
+                        Console.WriteLine(string.Join(" ", quantityOfOrders[j]));
+                    }
+                    return;
+                }
+            }
+
+            Console.WriteLine(orders.Max());
+
+            if (quantityOfFood >= orders.Sum())
+            {
+                Console.WriteLine("Orders complete");
+            }
+            
+        }
+    }
+}
