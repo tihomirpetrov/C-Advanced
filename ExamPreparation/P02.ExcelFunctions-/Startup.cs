3d867b2bf6e0bf85f5aa712f3271405615201cde
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-19 22:25:21 +0200

    initial commit

--- ExamPreparation/P02.ExcelFunctions-/Startup.cs
+++ ExamPreparation/P02.ExcelFunctions-/Startup.cs
@@ -21,18 +21,47 @@
             string[] commandArgs = Console.ReadLine().Split();
             string command = commandArgs[0];
             string header = commandArgs[1];
+            int headerIndex = Array.IndexOf(table[0], header);
 
             if (command == "hide")
             {
-                int headerIndex = Array.IndexOf(table[0], header);
-                
+
                 for (int row = 0; row < table.Length; row++)
                 {
-                    List<string> lineToPrint = new List<string>();
+                    List<string> lineToPrint = new List<string>(table[row]);
+
+                    lineToPrint.RemoveAt(headerIndex);
+
+                    //Console.WriteLine(string.Join(" | ",table[row].Where((x, i) => i != headerIndex).ToArray()));
                     lineToPrint.AddRange(table[row].Take(headerIndex).ToList());
                     lineToPrint.AddRange(table[row].Skip(headerIndex + 1));
                     Console.WriteLine(string.Join(" | ", lineToPrint));
+
+                    table[row] = lineToPrint.ToArray();
                 }
+            }
+
+            else if (command == "sort")
+            {
+                string[] headerRow = table[0];
+
+                Console.WriteLine(string.Join(" | ", headerRow));
+
+                table = table.OrderBy(x => x[headerIndex]).ToArray();
+
+                foreach (var row in table)
+                {
+                    if (row != headerRow)
+                    {
+                        Console.WriteLine(string.Join(" | ", row));
+                    }
+                }
+            }
+
+            else if (command == "filter")
+            {
+                string value = commandArgs[2];
+
             }
         }
     }
