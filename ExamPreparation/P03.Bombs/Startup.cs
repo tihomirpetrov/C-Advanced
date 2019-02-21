920fd399073191708fcb4aa2cf702c200aa26f28
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-21 17:51:02 +0200

    initial commit

---
+++ ExamPreparation/P03.Bombs/Startup.cs
@@ -0,0 +1,115 @@
+﻿namespace P03.Bombs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            int sizeOfMatrix = int.Parse(Console.ReadLine());
+            int[][] matrix = new int[sizeOfMatrix][];
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                matrix[row] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            }
+
+            string[] coordinates = Console.ReadLine().Split(" ").ToArray();
+            Dictionary<int, int> bombIndexes = new Dictionary<int, int>();
+
+            for (int i = 0; i < coordinates.Length; i++)
+            {
+                int[] cellsWithBombs = coordinates[i].Split(",").Select(int.Parse).ToArray();
+                int bombRow = cellsWithBombs[0];
+                int bombCol = cellsWithBombs[1];
+                int bombNumber = matrix[bombRow][bombCol];
+
+                if (!bombIndexes.ContainsKey(bombRow))
+                {
+                    bombIndexes.Add(bombRow, -1);
+                }
+                bombIndexes[bombRow] = bombCol;
+
+
+                if (bombIndexes.ContainsKey(bombRow) && bombIndexes.ContainsValue(bombCol))
+                {
+                    matrix[bombRow][bombCol] = 0;
+                }
+
+                if (bombRow - 1 < 0 || bombRow + 1 > matrix.Length - 1)
+                {
+                    continue;
+                }
+                else
+                {
+                    if (matrix[bombRow - 1][bombCol] > 0)
+                    {
+                        matrix[bombRow - 1][bombCol] -= bombNumber;
+                    }
+                    if (matrix[bombRow + 1][bombCol] > 0)
+                    {
+                        matrix[bombRow + 1][bombCol] -= bombNumber;
+                    }
+
+                    if (bombCol - 1 >= 0 && matrix[bombRow - 1][bombCol - 1] >= 0)
+                    {
+                        matrix[bombRow - 1][bombCol - 1] -= bombNumber;
+                    }
+
+                    if (bombCol + 1 <= matrix.Length && matrix[bombRow + 1][bombCol + 1] > 0)
+                    {
+                        matrix[bombRow + 1][bombCol + 1] -= bombNumber;
+                    }
+                }
+
+                //if (bombCol - 1 < 0 || bombCol + 1 > 0)
+                //{
+
+                //}
+                if (matrix[bombRow - 1][bombCol + 1] > 0)
+                {
+                    matrix[bombRow - 1][bombCol + 1] -= bombNumber;
+                }
+                if (bombCol > 0)
+                {
+                    if (matrix[bombRow][bombCol - 1] > 0)
+                    {
+                        matrix[bombRow][bombCol - 1] -= bombNumber;
+                    }
+                    if (matrix[bombRow + 1][bombCol - 1] > 0)
+                    {
+                        matrix[bombRow + 1][bombCol - 1] -= bombNumber;
+                    }
+                }
+                if (matrix[bombRow][bombCol + 1] > 0)
+                {
+                    matrix[bombRow][bombCol + 1] -= bombNumber;
+                }
+                
+            }
+
+            int count = 0;
+            int sum = 0;
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                for (int col = 0; col < matrix.Length; col++)
+                {
+                    if (matrix[row][col] > 0)
+                    {
+                        sum += matrix[row][col];
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {count}");
+            Console.WriteLine($"Sum: {sum}");
+            foreach (var item in matrix)
+            {
+                Console.WriteLine(string.Join(" ", item));
+            }
+        }
+    }
+}
