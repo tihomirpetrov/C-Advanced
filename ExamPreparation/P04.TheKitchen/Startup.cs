5565ee8f3f024bd6fa3a8eba079b8f8372ee2773
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-21 20:09:28 +0200

    initial commit

---
+++ ExamPreparation/P04.TheKitchen/Startup.cs
@@ -0,0 +1,39 @@
+﻿namespace P04.TheKitchen
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            List<int> knives = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> forks = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> sets = new List<int>();           
+
+            while (knives.Count > 0 && forks.Count > 0)
+            {
+                if (knives[knives.Count - 1] > forks[0])
+                {
+                    sets.Add(knives[knives.Count - 1] + forks[0]);
+                    knives.RemoveAt(knives.Count - 1);
+                    forks.RemoveAt(0);
+                }
+                else if (knives[knives.Count - 1] < forks[0])
+                {
+                    knives.RemoveAt(knives.Count - 1);
+                    continue;
+                }
+                else if(knives[knives.Count - 1] == forks[0])
+                {
+                    forks.RemoveAt(0);
+                    knives[knives.Count - 1]++;
+                }
+            }
+
+            Console.WriteLine($"The biggest set is: {sets.Max()}");
+            Console.WriteLine(string.Join(" ", sets));
+        }
+    }
+}
