76bdd1d135ae27cbed4978c2383b270be5ecbe91
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-22 15:04:24 +0200

    initial commit

--- ExamPreparation/P03.Miner/Startup.cs
+++ ExamPreparation/P03.Miner/Startup.cs
@@ -13,11 +13,15 @@
 
             for (int row = 0; row < fieldSize; row++)
             {
-                gameField[row] = Console.ReadLine().ToCharArray();
+                string line = Console.ReadLine();
+                string temp = line.Replace(" ", "");
+                gameField[row] = temp.ToCharArray();
             }
 
             int minerRow = -1;
             int minerCol = -1;
+            int coalsFound = 0;
+                    
             for (int row = 0; row < gameField.Length; row++)
             {
                 if (gameField[row].Contains('s'))
@@ -25,40 +29,73 @@
                     minerRow = row;
                     minerCol = Array.IndexOf(gameField[row], 's');
                 }
-            }
-            
 
+                if (gameField[row].Contains('c'))
+                {                    
+                    for (int col = 0; col < gameField[row].Length; col++)
+                    {
+                        if (gameField[row][col] == 'c')
+                        {
+                            coalsFound++;
+                        }
+                    }
+                }
+            }
 
             for (int i = 0; i < commands.Length; i++)
             {
 
                 if (commands[i] == "up")
                 {
-
+                    if (minerRow == 0)
+                    {
+                        continue;
+                    }
+                    minerRow--;
                 }
                 else if (commands[i] == "down")
                 {
-
+                    if (minerRow == gameField.Length - 1)
+                    {
+                        continue;
+                    }
+                    minerRow++;
                 }
                 else if (commands[i] == "left")
                 {
-
+                    if (minerCol == 0)
+                    {
+                        continue;
+                    }
+                    minerCol--;
                 }
                 else if (commands[i] == "right")
                 {
-
+                    if (minerCol == gameField.Length - 1)
+                    {
+                        continue;
+                    }
+                    minerCol++;
                 }
 
-
+                if (gameField[minerRow][minerCol] == 'e')
+                {
+                    Console.WriteLine($"Game over! ({minerRow}, {minerCol})");
+                    return;
+                }
+                else if (gameField[minerRow][minerCol] == 'c')
+                {
+                    coalsFound--;
+                    gameField[minerRow][minerCol] = '*';
+                    if (coalsFound == 0)
+                    {
+                        Console.WriteLine($"You collected all coals! ({minerRow}, {minerCol})");
+                        return;
+                    }                    
+                }
             }
-
-
-            //it's just for test where is the miner
-            //should be deleted before submit to judge
-            foreach (var item in gameField)
-            {
-                Console.WriteLine(string.Join("", item));
-            }
+           
+            Console.WriteLine($"{coalsFound} coals left. ({minerRow}, {minerCol})");
         }
     }
 }
