4f743e90bc7457b1bc47aeb8399e11809d1844f7
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-23 04:16:47 +0200

    initial commit

---
+++ ExamPreparation/P01.Crossroads/Startup.cs
@@ -0,0 +1,25 @@
+﻿namespace P01.Crossroads
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            Queue<string> cars = new Queue<string>();
+            int greenLight = int.Parse(Console.ReadLine());
+            int freeWindows = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+
+
+                input = Console.ReadLine();
+            }
+
+        }
+    }
+}
