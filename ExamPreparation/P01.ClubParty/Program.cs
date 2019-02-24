711ed91223301b6edfcf7e2b4f85976ca3a78cbf
tihomirpetrov <tihomirpetrov79@gmail.com>
2019-02-24 10:14:28 +0200

    initial commit

---
+++ ExamPreparation/P01.ClubParty/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace P01.ClubParty
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int maximumCapacity = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            Dictionary<string, Dictionary<int, int>> hallReservationsPeople = new Dictionary<string, Dictionary<int, int>>();
+
+            //List<string> halls = new List<string>();
+            //List<int> people = new List<int>();
+            Stack<string> tokens = new Stack<string>(input);
+
+            while (maximumCapacity > 0 || tokens.Count > 0)
+            {
+                string hall = tokens.Pop();
+                int reservation = int.Parse(tokens.Pop());
+                int peopleIn = int.Parse(tokens.Pop());
+
+
+                if (peopleIn + reservation <= maximumCapacity)
+                {
+                    if (!hallReservationsPeople.ContainsKey(hall))
+                    {
+                        hallReservationsPeople.Add(hall, new Dictionary<int, int>());
+                    }
+                    if (!hallReservationsPeople[hall].ContainsKey(reservation))
+                    {
+                        hallReservationsPeople[hall].Add(reservation, 0);
+                    }
+                    hallReservationsPeople[hall][reservation] = peopleIn;
+
+                    maximumCapacity -= reservation + peopleIn;
+                }
+                else
+                {
+
+                    if (!hallReservationsPeople.ContainsKey(hall))
+                    {
+                        hallReservationsPeople.Add(hall, new Dictionary<int, int>());
+                        if (!hallReservationsPeople[hall].ContainsKey(reservation))
+                        {
+                            hallReservationsPeople[hall][reservation] = peopleIn;
+
+                        }
+
+                        if (hallReservationsPeople[hall][reservation] > maximumCapacity - reservation)
+                        {
+                            hallReservationsPeople[hall][reservation] = peopleIn;
+                        }
+                        else
+                        {
+                            hallReservationsPeople.Add(hall, new Dictionary<int, int>());
+                        }
+                    }
+                    if (!hallReservationsPeople[hall].ContainsKey(reservation))
+                    {
+                        hallReservationsPeople[hall].Add(reservation, 0);
+                    }
+                    hallReservationsPeople[hall][reservation] = peopleIn;
+                                       
+                    maximumCapacity -= reservation + peopleIn;
+                }
+
+            }
+
+
+        }
+    }
+}
--- ExamPreparation/P01.Crossroads/Startup.cs
+++ ExamPreparation/P01.Crossroads/Startup.cs
@@ -8,18 +8,53 @@
         public static void Main()
         {
             Queue<string> cars = new Queue<string>();
+            Stack<string> carsPassed = new Stack<string>();
             int greenLight = int.Parse(Console.ReadLine());
-            int freeWindows = int.Parse(Console.ReadLine());
+            int currentGreenLight = greenLight;
+            int freeWindow = int.Parse(Console.ReadLine());
 
             string input = Console.ReadLine();
 
             while (input != "END")
             {
+                if (input == "green")
+                {
+                    for (int i = 0; i < greenLight; i++)
+                    {
+                        if (cars.Count == 0)
+                        {
+                            greenLight = currentGreenLight;
+                            break;
+                        }
 
+                        string car = cars.Peek();
+
+                        if (car.Length < greenLight + freeWindow)
+                        {
+                            cars.Dequeue();
+                            carsPassed.Push(car);
+                            greenLight -= car.Length;
+                        }
+                        else
+                        {
+                            int carHitIndex = Math.Abs(car.Length - 1 - greenLight);
+                            char characterHit = car[carHitIndex];
+                            Console.WriteLine("A crash happened!");
+                            Console.WriteLine($"{car} was hit at {characterHit}.");
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(input);
+                }
 
                 input = Console.ReadLine();
             }
 
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine($"{carsPassed.Count} total cars passed the crossroads.");
         }
     }
 }
